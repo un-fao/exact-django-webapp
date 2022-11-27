e913d800d1e9d0a2f2b1583309fc73d9e6a6583e
sirvosterzo <lavacca.c@gmail.com>
2022-11-27 22:41:04 +0100

    Moved IPCC data in its own app

--- djangoexact/djangoexact/settings.py
+++ djangoexact/djangoexact/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django_extensions',
 
     'rest_framework',
+    'ipcc',
     'api',
 ]
 
---
+++ djangoexact/ipcc/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IpccConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ipcc'
