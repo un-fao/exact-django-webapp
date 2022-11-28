e914424cbbfca9161c4b9e78175664663b31657c
sirvosterzo <lavacca.c@gmail.com>
2022-11-28 08:37:18 +0100

    Updated requirements

--- djangoexact/djangoexact/urls.py
+++ djangoexact/djangoexact/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/clearcache/', include('clearcache.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/ipcc/', include('ipcc.urls')),
