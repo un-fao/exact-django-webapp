28e421d3b6bbde3585f8fd2c0df43302bd965739
sirvosterzo <lavacca.c@gmail.com>
2022-11-28 01:48:42 +0100

    Added generic fetch API for IPCC data

--- djangoexact/djangoexact/urls.py
+++ djangoexact/djangoexact/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/clearcache/', include('clearcache.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api/ipcc/', include('ipcc.urls')),
 ]
