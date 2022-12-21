cbf1f10f64e5f6865f0a88ab712fd1c763b09736
sirvosterzo <lavacca.c@gmail.com>
2022-12-22 00:53:38 +0100

    Cut superfluous serializers

--- djangoexact/api/serializers.py
+++ djangoexact/api/serializers.py
@@ -26,22 +26,11 @@ def getResultsSerializer(model_arg):
 
     return GenericResultsSerializer
 
-def getModelSerializer(model_arg):
+def get_model_serializer(model_arg):
     class GenericSerializer(serializers.ModelSerializer):
         class Meta:
             model = model_arg
             fields = '__all__'
+            ref_name = model_arg.__name__
 
     return GenericSerializer
-
-def getAssessmentSerializer(model_arg):
-    class AssessmentSerializer(serializers.Serializer):
-        parent_name = serializers.CharField(max_length=200)
-        parent_id = serializers.IntegerField()
-        assessment = getModelSerializer(model_arg)()
-
-        def save(self):
-            self.assessment.save()
-            return self.assessment
-
-    return AssessmentSerializer
