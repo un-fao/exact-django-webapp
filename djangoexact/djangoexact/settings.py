709a65c48082f909485dd453b878ccf6e1e03182
sirvosterzo <lavacca.c@gmail.com>
2022-12-21 20:42:26 +0100

    Removed useless apps

--- djangoexact/djangoexact/settings.py
+++ djangoexact/djangoexact/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
 
-    'polymorphic',
-
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
