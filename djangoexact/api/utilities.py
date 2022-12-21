a392d3d94bf946edbb81fc3ec432492322a05d0e
sirvosterzo <lavacca.c@gmail.com>
2022-12-22 00:55:43 +0100

    Add generic Module Viewset

--- djangoexact/api/utilities.py
+++ djangoexact/api/utilities.py
@@ -1,4 +1,5 @@
 from django.apps import apps
+from rest_framework import exceptions
 
 CN_RATIO_FOREST = 10
 CN_RATIO_GRASSLAND = 15
@@ -25,3 +26,9 @@ def error(msg):
 
 def get_model(name, app_name='api', suffix='Input'):
     return apps.get_model(app_name, sanitize_for_model(name+suffix))
+
+def get_query_param_or_validation_error(request, param_name):
+    param = request.query_params.get(param_name)
+    if param is None:
+        raise exceptions.ValidationError(f"{param_name} is required")
+    return param
