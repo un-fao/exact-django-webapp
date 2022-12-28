6144e58f328daac95def29f962aa5d4833113d41
sirvosterzo <lavacca.c@gmail.com>
2022-12-28 21:44:55 +0100

    Cut useless serializers

--- djangoexact/api/serializers.py
+++ djangoexact/api/serializers.py
@@ -1,30 +1,13 @@
 from rest_framework import serializers
 from .models import *
 
-def getResultSerializer():
+def get_result_serializer():
     class ResultSerializer(serializers.Serializer):
         total_w = serializers.FloatField()
         total_wo = serializers.FloatField()
         balance = serializers.FloatField()
     
     return ResultSerializer
-
-def getResultsSerializer(model_arg):
-
-    class GenericSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = model_arg
-            fields = '__all__'
-
-    class GenericResultInputsSerializer(serializers.Serializer):
-        input = GenericSerializer()
-        result = getResultSerializer()
-
-    class GenericResultsSerializer(serializers.Serializer):
-        inputs = GenericResultInputsSerializer(many=True)
-        result = getResultSerializer()
-
-    return GenericResultsSerializer
 
 def get_model_serializer(model_arg):
     class GenericSerializer(serializers.ModelSerializer):
