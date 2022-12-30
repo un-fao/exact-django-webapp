0888b02b2557f9a4ec6dd884769a4be6522d1cf5
sirvosterzo <lavacca.c@gmail.com>
2022-12-30 16:25:32 +0100

    Fix bug in get_module_serializer

--- djangoexact/api/serializers.py
+++ djangoexact/api/serializers.py
@@ -21,9 +21,10 @@ def get_module_serializer(model_arg):
     class GenericSerializer(serializers.ModelSerializer):
 
         # activity = serializers.PrimaryKeyRelatedField(source='activity.name', queryset=Activity.objects.all(), many=False)
-        
+
         class Meta:
             model = model_arg
             fields = '__all__'
+            ref_name = model_arg.__name__
 
     return GenericSerializer
