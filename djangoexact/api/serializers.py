5e6c1da16137585e551e14e7edb8821b36dd27b2
sirvosterzo <lavacca.c@gmail.com>
2022-12-30 16:19:14 +0100

    Add model/module serializer differentiation
    Added for future developments.

--- djangoexact/api/serializers.py
+++ djangoexact/api/serializers.py
@@ -15,5 +15,15 @@ def get_model_serializer(model_arg):
             model = model_arg
             fields = '__all__'
             ref_name = model_arg.__name__
+    return GenericSerializer
+
+def get_module_serializer(model_arg):
+    class GenericSerializer(serializers.ModelSerializer):
+
+        # activity = serializers.PrimaryKeyRelatedField(source='activity.name', queryset=Activity.objects.all(), many=False)
+        
+        class Meta:
+            model = model_arg
+            fields = '__all__'
 
     return GenericSerializer
