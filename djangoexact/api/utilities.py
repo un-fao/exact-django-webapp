2cf4cf662d0c9aec2f002199ba03c98ceae98545
sirvosterzo <lavacca.c@gmail.com>
2023-03-03 17:09:16 +0100

    Add some comments

--- djangoexact/api/utilities.py
+++ djangoexact/api/utilities.py
@@ -50,12 +50,14 @@ def get_assessment_or_parent(module) -> tuple[Model, str] | tuple[None, None]:
     relative = (None, None)
 
     # NOTE: To add new assessments, make sure you follow the naming convention of the Assessment class in models.py
+    # NOTE: Refer to documentation for more information (WIP)
     for attr in dir(module):
         if getattr(module, attr, None) is not None:
             
             if "_assessment" in attr:
                 relative = (getattr(module, attr), 'child')
                 break
+            # Matches all attributes with 'parent_' prefix and not ending with '_id'
             elif re.match('parent_[^_]*[^_id]', attr):
                 relative = (getattr(module, attr), 'parent')
                 break
