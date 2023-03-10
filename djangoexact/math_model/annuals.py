4d61b4b23d1f4bbddbb1ecf9d85e203077de4714
sirvosterzo <lavacca.c@gmail.com>
2023-03-10 14:10:55 +0100

    Fix bug in calculations

--- djangoexact/math_model/annuals.py
+++ djangoexact/math_model/annuals.py
@@ -129,9 +129,9 @@ def soil_co2_change(area_start, area, time_impl, time_cap, rate_coefficient, rat
     delta_soil_c_20_years = delta_soil_c / 20
 
     maximum = - delta_soil_c * max(area_start, area)
-    calculated_time_ind = - delta_soil_c_20_years * min(area, area_start) * min (20, time_impl + time_cap)
+    calculated_time_ind = min(area, area_start) * min (20, time_impl + time_cap)
     calculated_time_dep = immediate_soil(area, area_start, time_impl, time_cap) if rate_type == 'I' else not_immediate_soil(area, area_start, time_impl, time_cap, rate_type, rate_coefficient)
-    calculated = calculated_time_dep + calculated_time_ind
+    calculated = (calculated_time_dep + calculated_time_ind) * (- delta_soil_c_20_years) 
 
 
     return maximum if abs(calculated) > maximum else calculated
@@ -159,7 +159,7 @@ def som (area_start, area, time_impl, time_cap, rate_coefficient, socref, soc_ti
     n2o_n_conversion = 44/28
 
 
-    som_n2o = 0 if maximum_soc_20_years > reference_soc else (maximum_soc_20_years - reference_soc) * 5  * emission_factor_nitrous * n2o_n_conversion * (nitrous_constant/1000)
+    som_n2o = 0 if maximum_soc_20_years < reference_soc else (maximum_soc_20_years - reference_soc) * 5  * emission_factor_nitrous * n2o_n_conversion * (nitrous_constant/1000)
 
     total = - (min(area_start, area) * (time_cap + time_impl) + abs(area - area_start) * func1(area_start, area, rate_coefficient, time_impl, time_cap)) *  som_n2o 
 
@@ -184,13 +184,17 @@ def residue_burning(area_start, area, time_impl, time_cap, rate_coefficient, nit
     # ACTUAL COMPUTATION
 
     ################## COMPUTATION OF AMOUNT OF KG OF METHANE ###################
-    ag_residue_main = residue_main_tier_2 * 1000 if residue_main_tier_2 else yield_value_main * 1000 * n_estimation_slope_main + n_estimation_intercept_main
+
+    yield_value_main = yield_value_main * 1000
+    yield_value_minor = yield_value_minor * 1000 if yield_value_minor else None
+
+    ag_residue_main = residue_main_tier_2 * 1000 if residue_main_tier_2 else yield_value_main * n_estimation_slope_main + n_estimation_intercept_main
     ag_residue_tonnes_main = ag_residue_main / 1000
     if ef_methane_agr_residues_main: 
         main_season_methane = ag_residue_tonnes_main * ef_methane_agr_residues_main * combustion_factor_main
     else:
         main_season_methane = 0
-    ag_residue_minor = residue_minor_tier_2 * 1000 if residue_minor_tier_2 else yield_value_minor * 1000 * n_estimation_slope_minor + n_estimation_intercept_minor if yield_value_minor else 0
+    ag_residue_minor = residue_minor_tier_2 * 1000 if residue_minor_tier_2 else yield_value_minor * n_estimation_slope_minor + n_estimation_intercept_minor if yield_value_minor else 0
     ag_residue_tonnes_minor = ag_residue_minor / 1000 
     if ef_methane_agr_residues_minor:
         minor_season_methane = ag_residue_tonnes_minor * ef_methane_agr_residues_minor * combustion_factor_minor
@@ -231,7 +235,12 @@ def residue_burning(area_start, area, time_impl, time_cap, rate_coefficient, nit
 
     co2_crop = (kg_nitrous * nitrous_constant + kg_methane * methane_constant)/1000
 
-    total = (min(area_start, area) * (time_cap + time_impl) + abs(area - area_start) * func1(area_start, area, rate_coefficient, time_impl, time_cap)) * co2_crop
+    _min = min(area_start, area) * (time_cap + time_impl)
+    _abs = abs(area - area_start) * func1(area_start, area, rate_coefficient, time_impl, time_cap)
+
+    min__abs = _min + _abs
+
+    total = (min__abs) * co2_crop
 
     return total
 
