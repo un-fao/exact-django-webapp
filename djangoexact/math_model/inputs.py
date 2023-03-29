feb038cd893287c1cc40501d6ca7b1c75a69f2b9
Peter Anthony Wright <peter.wright95@gmail.com>
2023-03-29 14:49:20 +0200

    Add inputs.py w_wo_calculation

--- djangoexact/math_model/inputs.py
+++ djangoexact/math_model/inputs.py
@@ -1,23 +1,33 @@
-def input_single_calculation(unit_start, unit_end, rate_coefficient, ipcc_factor, tier_2_factor, unit_factor, emissions_factor, time_impl, time_cap):
+def input_w_wo_calculation(unit_start, unit_w, unit_wo, rate_coefficient_w, rate_coefficient_wo, ipcc_factor, tier_2_factor, unit_factor, emissions_factor, time_impl, time_cap):
+    
+    
+    def input_single_calculation(unit_start, unit_end, rate_coefficient, ipcc_factor, tier_2_factor, unit_factor, emissions_factor, time_impl, time_cap):
 
-    def time_dependency(area_start, area, rate_coefficient, time_impl, time_cap):
+        def time_dependency(area_start, area, rate_coefficient, time_impl, time_cap):
 
-            if area > area_start:
-                return time_cap + time_impl * rate_coefficient
-            else:
-                return time_impl * (1 - rate_coefficient)
+                if area > area_start:
+                    return time_cap + time_impl * rate_coefficient
+                else:
+                    return time_impl * (1 - rate_coefficient)
             
-    ipcc_or_tier_2_factor = tier_2_factor if tier_2_factor else ipcc_factor
+        ipcc_or_tier_2_factor = tier_2_factor if tier_2_factor else ipcc_factor
 
-    unit_start = unit_start * unit_factor * ipcc_or_tier_2_factor
-    unit_end = unit_end * unit_factor * ipcc_or_tier_2_factor
+        unit_start = unit_start * unit_factor * ipcc_or_tier_2_factor
+        unit_end = unit_end * unit_factor * ipcc_or_tier_2_factor
 
-    emissions_start = unit_start * ipcc_factor * emissions_factor
-    emissions_end = unit_end * ipcc_factor * emissions_factor
+        emissions_start = unit_start * ipcc_factor * emissions_factor
+        emissions_end = unit_end * ipcc_factor * emissions_factor
 
-    total_emissions = (min(emissions_start, emissions_end) * (time_cap + time_impl) + abs(emissions_end - emissions_start) * time_dependency(emissions_start, emissions_end, rate_coefficient, time_impl, time_cap))
+        total_emissions = (min(emissions_start, emissions_end) * (time_cap + time_impl) + abs(emissions_end - emissions_start) * time_dependency(emissions_start, emissions_end, rate_coefficient, time_impl, time_cap))
 
-    return total_emissions
+        return total_emissions
+
+    
+    em_w = input_single_calculation(unit_start, unit_w, rate_coefficient_w, ipcc_factor, tier_2_factor, unit_factor, emissions_factor, time_impl, time_cap)
+    em_wo = input_single_calculation(unit_start, unit_wo, rate_coefficient_wo, ipcc_factor, tier_2_factor, unit_factor, emissions_factor, time_impl, time_cap)
+
+    return em_w, em_wo
+
 
 def new_irrigation_system(ef_ipcc, ef_tier_2, units_end):
      
@@ -43,13 +53,6 @@ def operational_irrigation(ef_default, ef_tier_2, total_dynamic_head_default, to
     electricity_multiplier_w: 10% if source_of_energy == 'electricity' else None
     electricity_multiplier_wo: 10% if source_of_energy == 'electricity' else None
     """
-
-    def time_dependency(area_start, area, rate_coefficient, time_impl, time_cap):
-
-            if area > area_start:
-                return time_cap + time_impl * rate_coefficient
-            else:
-                return time_impl * (1 - rate_coefficient)
 
     def ef_calculation(ef_default, ef_tier_2, total_dynamic_head_default, total_dynamic_head_tier_2, average_pressure_default, average_pressure_tier_2, pumping_efficiency_default, pumping_efficiency_tier_2, erh, depth ):
         pumping_efficiency = pumping_efficiency_default if not pumping_efficiency_tier_2 else pumping_efficiency_tier_2
