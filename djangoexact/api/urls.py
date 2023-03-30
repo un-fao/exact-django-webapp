6e1d4845beb8fb78eed481c58a2496e8e0f438a2
Claudio Lavacca <claudio.lavacca@fao.org>
2023-03-30 18:52:23 +0200

    Add calculations for Buildings And Roads

--- djangoexact/api/urls.py
+++ djangoexact/api/urls.py
@@ -49,6 +49,7 @@ router.register(r'small-fisheries', views.generic_module_viewset(SmallFishery), 
 router.register(r'large-fisheries', views.generic_module_viewset(LargeFishery), basename='large-fisheries')
 router.register(r'aquacultures', views.generic_module_viewset(Aquaculture), basename='aquacultures')
 router.register(r'inputs', views.generic_module_viewset(Input), basename='inputs')
+router.register(r'buildings', views.generic_module_viewset(Building), basename='buildings')
 
 urlpatterns = [
    path('docs/', include_docs_urls(title='EX-ACT Docs')),
