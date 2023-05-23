e25191f945cc37da39f7c5cd8c206ed4237eaf57
Peter Anthony Wright <peter.wright95@gmail.com>
2023-05-23 12:22:54 +0200

    Add Flooded Rice
    Version 1 created, added logic for calculation of soc changes, function
    has to be changed the same as for all cropland modules in future releases

--- djangoexact/math_model/flooded_rice.py
+++ djangoexact/math_model/flooded_rice.py
@@ -1,24 +1,38 @@
 import math
 
-def calculate_emissions(area_start, area_w, area_wo):
+def calculate_emissions(area_start, area_w, area_wo, EFc_ref, EFc_tier_2, SFw_ref, SFw_tier_2, SFp_ref, SFp_tier_2, SFo_ref, SFo_tier_2, yield_ref, rice_slope, rice_intercept, straw_tonnes_tier_2, methane_ef, rice_cf, nitrous_ef, nitrous_constant,
+                        time_impl, time_cap, rate_coefficient_w, methane_constant, cultivation_period_ref, cultivation_period_tier_2, rate_coefficient_wo, rate_type_w, rate_type_wo,  socref, soc_tier_2, f_lu_ref, f_lu_tier_2, f_i_ref, f_i_tier_2, f_mg_ref, f_mg_tier_2
+                        ):
 
     EFc = EFc_ref if not EFc_tier_2 else EFc_tier_2
     SFw = SFw_ref if not SFw_tier_2 else SFw_tier_2
     SFp = SFp_ref if not SFp_tier_2 else SFp_tier_2
     SFo = SFo_ref if not SFo_tier_2 else SFo_tier_2
-    rice_yield = yield_ref if not yield_tier_2 else yield_tier_2
+    rice_yield = yield_ref
 
     if area_start == 0 and area_w == 0 and area_wo == 0:
         adjusted_daily_ef_methane_ref = 0
         straw_tonnes_ref = 0
     else:
         adjusted_daily_ef_methane_ref = EFc * SFw * SFp * SFo
-        straw_tonnes_ref = rice_yield * rice_slope + rice_intercept
-
+        straw_tonnes_ref = rice_yield * rice_slope + rice_intercept if not straw_tonnes_tier_2 else straw_tonnes_tier_2
     
+    ch4_emitted_w = ch4_emitted(area_start, area_w, time_impl, time_cap, rate_coefficient_w, methane_constant, cultivation_period_ref, cultivation_period_tier_2, adjusted_daily_ef_methane_ref)
+    ch4_emitted_wo = ch4_emitted(area_start, area_wo, time_impl, time_cap, rate_coefficient_wo, methane_constant, cultivation_period_ref, cultivation_period_tier_2, adjusted_daily_ef_methane_ref)
+
+    straw_burning_w = straw_burning(area_start, area_w, time_impl, time_cap, rate_coefficient_w, straw_tonnes_ref, rice_cf, methane_ef, methane_constant, nitrous_ef, nitrous_constant, straw_tonnes_ref, straw_tonnes_tier_2)
+    straw_burning_wo = straw_burning(area_start, area_wo, time_impl, time_cap, rate_coefficient_wo, straw_tonnes_ref, rice_cf, methane_ef, methane_constant, nitrous_ef, nitrous_constant, straw_tonnes_ref, straw_tonnes_tier_2)
+
+    soil_co2_change_w = soil_co2_change(area_start, area_w, time_impl, time_cap, rate_coefficient_w, rate_type_w, socref, soc_tier_2, f_lu_ref, f_lu_tier_2, f_i_ref, f_i_tier_2, f_mg_ref, f_mg_tier_2)
+    soil_co2_change_wo = soil_co2_change(area_start, area_wo, time_impl, time_cap, rate_coefficient_wo, rate_type_wo, socref, soc_tier_2, f_lu_ref, f_lu_tier_2, f_i_ref, f_i_tier_2, f_mg_ref, f_mg_tier_2)
+
+    total_w = ch4_emitted_w + straw_burning_w + soil_co2_change_w
+    total_wo = ch4_emitted_wo + straw_burning_wo + soil_co2_change_wo
     
 
-    return
+    return total_w, total_wo, total_w - total_wo
+
+
 
 def soil_co2_change(area_start, area, time_impl, time_cap, rate_coefficient, rate_type, socref, soc_tier_2, f_lu_ref, f_lu_tier_2, f_i_ref, f_i_tier_2, f_mg_ref, f_mg_tier_2):
 
@@ -83,7 +97,7 @@ def soil_co2_change(area_start, area, time_impl, time_cap, rate_coefficient, rat
     return maximum if abs(calculated) > maximum else calculated
 
 def ch4_emitted(area_start, area, time_impl, time_cap, rate_coefficient, methane_constant, cultivation_period_ref, 
-                cultivation_period_tier_2, daily_ef_methane_default, daily_ef_methane_tier_2):
+                cultivation_period_tier_2, daily_ef_methane):
 
     def area_time_dependency(area_start, area, rate_coefficient, time_impl, time_cap):
         if area > area_start:
@@ -92,7 +106,6 @@ def ch4_emitted(area_start, area, time_impl, time_cap, rate_coefficient, methane
             return time_impl * (1 - rate_coefficient)
 
     cultivation_period = cultivation_period_ref if not cultivation_period_tier_2 else cultivation_period_tier_2
-    daily_ef_methane = daily_ef_methane_default if not daily_ef_methane_tier_2 else daily_ef_methane_tier_2
 
     kg_methane_cultivation_period = daily_ef_methane * cultivation_period
 
@@ -125,8 +138,11 @@ def straw_burning(area_start, area, time_impl, time_cap, rate_coefficient, straw
         
         return (min(area_start, area) * (time_cap + time_impl) + abs(area_start - area) * area_time_dependency(area_start, area, rate_coefficient)) * (straw_methane_co2 + straw_nitrous_co2)
 
-straw = straw_burning(100, 100, 20, 9, 0.5, True, 0.8, 2.7, 28, 0.07, 265, 45.2, None )
+# straw = straw_burning(100, 100, 20, 9, 0.5, True, 0.8, 2.7, 28, 0.07, 265, 45.2, None )
 
-ch4 = ch4_emitted(100, 100, 20, 9, 0.5, 28, 113, None, 1.05910000, None)
+# ch4 = ch4_emitted(100, 100, 20, 9, 0.5, 28, 113, None, 1.05910000, None)
 
-soil = soil_co2_change(100, 100, 20, 9, 'D', 0.5, 46, None, 1.35, None, )
+# soil = soil_co2_change(100, 100, 20, 9, 'D', 0.5, 46, None, 1.35, None, )
+
+em = calculate_emissions(12, 12, 12, 1.19, None, 1, None, 0.89, None, 1, None, 50, 0.95, 2.46, None,2.7, 0.8 ,0.07, 265, 20, 5, 0.5, 28, 113, None, 0.5, 'D', 'D', 70, None, 0, 0 , 0, 0, 0, 0)
+print(em)
