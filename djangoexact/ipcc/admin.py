bfb07c9af74095406b4799e0b04737a5e8cda121
Claudio Lavacca <claudio.lavacca@fao.org>
2023-07-03 17:58:54 +0200

    Start livestock results calculations

--- djangoexact/ipcc/admin.py
+++ djangoexact/ipcc/admin.py
@@ -1,21 +1,71 @@
 from django.contrib import admin
 from .models import *
 
-for model in [model for model in dir() if not model.startswith('_') and not model.startswith('PerennialAGB') and not model.startswith('PerennialBGB') and not model.startswith('AfforestationFLU')]:
+for model in [
+    model
+    for model in dir()
+    if not model.startswith("_")
+    and not model.startswith("PerennialAGB")
+    and not model.startswith("PerennialBGB")
+    and not model.startswith("AfforestationFLU")
+    and not model.startswith("LivestockTAM")
+    and not model.startswith("LivestockVSER")
+    and not model.startswith("LivestockManureEF")
+]:
     try:
         admin.site.register(eval(model))
     except:
         pass
 
+
 class AGBAdmin(admin.ModelAdmin):
-    search_fields = ['climate__name', 'moisture__name', 'land_use_type__name']
+    search_fields = ["climate__name", "moisture__name", "land_use_type__name"]
+
 
 class BGBAdmin(admin.ModelAdmin):
-    search_fields = ['climate__name', 'moisture__name', 'land_use_type__name', 'continent__name']
+    search_fields = [
+        "climate__name",
+        "moisture__name",
+        "land_use_type__name",
+        "continent__name",
+    ]
+
 
 class AfforestationFLUAdmin(admin.ModelAdmin):
-    search_fields = ['climate__name', 'moisture__name', 'land_use_type__name']
+    search_fields = ["climate__name", "moisture__name", "land_use_type__name"]
+
+
+class LivestockTAMAdmin(admin.ModelAdmin):
+    search_fields = [
+        "emission_type__name",
+        "livestock_production_type__name",
+        "livestock_category_type__name",
+        "ipcc_region__name",
+    ]
+
+
+class LivestockVSERAdmin(admin.ModelAdmin):
+    search_fields = [
+        "emission_type__name",
+        "livestock_production_type__name",
+        "livestock_category_type__name",
+        "ipcc_region__name",
+    ]
+
+
+class LivestockManureEFAdmin(admin.ModelAdmin):
+    search_fields = [
+        "emission_type__name",
+        "livestock_production_type__name",
+        "livestock_category_type__name",
+        "climate__name",
+        "moisture__name",
+    ]
+
 
 admin.site.register(PerennialAGB, AGBAdmin)
 admin.site.register(PerennialBGB, BGBAdmin)
 admin.site.register(AfforestationFLU, AfforestationFLUAdmin)
+admin.site.register(LivestockTAM, LivestockTAMAdmin)
+admin.site.register(LivestockVSER, LivestockVSERAdmin)
+admin.site.register(LivestockManureEF, LivestockManureEFAdmin)
