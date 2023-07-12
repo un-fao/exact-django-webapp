f649ebd8edf251c2bf427d4cf5e509b96a860648
Claudio Lavacca <claudio.lavacca@fao.org>
2023-07-12 11:34:29 +0200

    Refactor ipcc urls

--- djangoexact/ipcc/serializers.py
+++ djangoexact/ipcc/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 
 # Template class for serializing
-class GeneralSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = '__all__'
+
+
+def get_model_serializer(model_arg):
+    class GenericSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = model_arg
+            fields = "__all__"
+            ref_name = model_arg.__name__
+
+    return GenericSerializer
