8ba25ec7186d15ab07bcdb09f02b08781f3c50e4
Claudio Lavacca <claudio.lavacca@fao.org>
2023-07-12 11:34:29 +0200

    Refactor ipcc urls

--- djangoexact/ipcc/urls.py
+++ djangoexact/ipcc/urls.py
@@ -1,9 +1,15 @@
-from . import views
 from django.apps import apps
-from django.urls import path
+from .views import generic_viewset
+from rest_framework import routers
+from .utilities import get_url_name
 
-# Create default router and register viewsets
+router = routers.DefaultRouter()
+
+
 urlpatterns = []
 
-for model_name, model in apps.get_app_config('ipcc').models.items():
-    urlpatterns.append(path(model_name, views.GenericAPIView.as_view(), {'model': model}))
+for model_name, model in apps.get_app_config("ipcc").models.items():
+    url_name = get_url_name(model.__name__)
+    router.register(url_name, generic_viewset(model), basename=url_name)
+
+urlpatterns += router.urls
