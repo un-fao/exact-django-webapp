5d8dcbc3e3911b4a913082bfbad03cffe776a215
Claudio Lavacca <claudio.lavacca@fao.org>
2023-07-17 12:05:34 +0200

    Fix bugs in AnnualCropland
    Add AnnualCropland unit tests
    Fix bug in project/results response json

--- djangoexact/api/factories.py
+++ djangoexact/api/factories.py
@@ -11,6 +11,11 @@ fishery_types = [fishery for fishery in FisheryType.objects.all()]
 fish_types = [fish for fish in FishType.objects.all()]
 statuses = [status for status in ProjectStatus.objects.all()]
 
+crop_types = [crop for crop in CropType.objects.filter(is_main_crop=True)]
+tillage_management_types = [tillage for tillage in TillageManagementType.objects.all()]
+organic_input_types = [organic for organic in OrganicInputType.objects.all()]
+residue_management_types = [residue for residue in ResidueManagementType.objects.all()]
+
 def_rate = ChangeRate.objects.get(name="D")
 
 
@@ -94,3 +99,69 @@ class LargeFisheryFactory(FisheryFactory):
     gear_type_start = lge_gear_type
     gear_type_w = lge_gear_type
     gear_type_wo = lge_gear_type
+
+
+class AnnualCroppingFactory(DjangoModelFactory):
+    class Meta:
+        model = AnnualCropping
+
+    """
+    crop_type = ForeignKey(CropType, on_delete=CASCADE, null=True, blank=True)
+    tillage_management_type = ForeignKey(TillageManagementType, on_delete=CASCADE)
+    organic_input_type = ForeignKey(OrganicInputType, on_delete=CASCADE)
+    residue_management_type = ForeignKey(ResidueManagementType, on_delete=CASCADE)
+    crop_yield = FloatField(null=True, blank=True)
+
+    ha_start = FloatField(null=True, blank=True)
+    ha_w = FloatField(null=True, blank=True)
+    ha_w_rate = ForeignKey(
+        ChangeRate,
+        on_delete=CASCADE,
+        related_name="%(class)s_ha_w_rate",
+        null=True,
+        blank=True,
+    )
+    ha_wo = FloatField(null=True, blank=True)
+    ha_wo_rate = ForeignKey(
+        ChangeRate,
+        on_delete=CASCADE,
+        related_name="%(class)s_ha_wo_rate+",
+        null=True,
+        blank=True,
+    )
+
+    main_soil_carbon_t2 = FloatField(null=True, blank=True)
+    main_tillage_factor_t2 = FloatField(null=True, blank=True)
+    main_organic_input_factor_t2 = FloatField(null=True, blank=True)
+    main_biomass_factor_t2 = FloatField(null=True, blank=True)
+
+    main_land_use_factor_t2 = FloatField(null=True, blank=True)
+
+    minor_crop_type_t2 = ForeignKey(
+        CropType,
+        on_delete=CASCADE,
+        null=True,
+        blank=True,
+        related_name="%(class)s_minor_crop_type",
+    )
+    minor_yield_t2 = FloatField(null=True, blank=True)
+    minor_residue_management_type_t2 = ForeignKey(
+        ResidueManagementType,
+        on_delete=CASCADE,
+        null=True,
+        blank=True,
+        related_name="%(class)s_minor_residue_management_type",
+    )
+    minor_biomass_factor_t2 = FloatField(null=True, blank=True)
+    """
+
+    crop_type = factory.fuzzy.FuzzyChoice(crop_types)
+    tillage_management_type = factory.fuzzy.FuzzyChoice(tillage_management_types)
+    organic_input_type = factory.fuzzy.FuzzyChoice(organic_input_types)
+    residue_management_type = factory.fuzzy.FuzzyChoice(residue_management_types)
+    crop_yield = factory.fuzzy.FuzzyInteger(0.0, 100)
+    ha_start = factory.fuzzy.FuzzyInteger(0.0, 100)
+    ha_w = factory.fuzzy.FuzzyInteger(0.0, 100)
+    ha_w_rate = def_rate
+    ha_wo = factory.fuzzy.FuzzyInteger(0.0, 100)
+    ha_wo_rate = def_rate
