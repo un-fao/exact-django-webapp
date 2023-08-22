5d6b2674dbfa2d69958252869e842ac2833bbffc
Peter Anthony Wright <peter.wright95@gmail.com>
2023-08-22 14:42:34 +0200

    Indentation Fixes

--- djangoexact/math_model/no_time_dependency_final/general_functions.py
+++ djangoexact/math_model/no_time_dependency_final/general_functions.py
@@ -89,6 +89,7 @@ def breakdown_according_to_values(maximum, list_of_proportions):
 
     result = [maximum * i/sum(list_of_proportions) for i in list_of_proportions]
     return result
+
 # LIVESTOCK CH4 HEAD GENERAL FUNCTION    
 def ch4_head_calculation_general(tam: float, vser: float, ef_prp: float, percentage_prp_default: float, percentage_prp_tier_2: float, ef_system_default: list, ch4_prp_tier_2: float, percentage_system_default: list, ef_single_system, ch4_system_tier_2, ch4_dividing_parameter = 1):
         
@@ -164,7 +165,6 @@ def soil_emissions_delta_soc_known(delta_soil_c, delta_soil_c_20_years, area_sta
     emissions_soil_total = emissions
 
     return emissions_soil_yearly, emissions_soil_total
-     
      
 # INPUT SINGLE MODULE CALCULATION
 def input_single_calculation(unit_start, unit_end, ipcc_factor, tier_2_factor, unit_factor, emissions_factor, time_implementation, time_capitalization, rate_type):
