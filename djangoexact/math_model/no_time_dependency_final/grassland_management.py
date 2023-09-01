e2fa86d4f7a64bfc2128adf1cf9d587d936d69f9
Peter Anthony Wright <peter.wright95@gmail.com>
2023-09-01 16:02:38 +0200

    Fix Grassland

--- djangoexact/math_model/no_time_dependency_final/grassland_management.py
+++ djangoexact/math_model/no_time_dependency_final/grassland_management.py
@@ -93,7 +93,7 @@ class GrasslandManagement:
         calculate_soil_emissions()
 
         try:
-            self.emissions_total_yearly = [x + y for x, y in zip(self.emissions_residue_burning_yearly, self.emissions_residue_burning_yearly)]
+            self.emissions_total_yearly = [x + y for x, y in zip(self.emissions_residue_burning_yearly, self.emissions_soil_yearly)]
             self.total_emissions = sum(self.emissions_total_yearly)
             return self.total_emissions
 
