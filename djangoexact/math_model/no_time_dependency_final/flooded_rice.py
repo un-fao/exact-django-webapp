7a59ce8023fbae86c143aba44f6ae6d726f9f7e4
Peter Anthony Wright <peter.wright95@gmail.com>
2023-09-06 12:16:08 +0200

    Fix flooded rice

--- djangoexact/math_model/no_time_dependency_final/flooded_rice.py
+++ djangoexact/math_model/no_time_dependency_final/flooded_rice.py
@@ -4,7 +4,7 @@ import traceback
 
 class FloodedRice:
 
-    def __init__(self, area_start, area_end, EFc_ref, EFc_tier_2, SFw_ref, SFw_tier_2, SFp_ref, SFp_tier_2, cfoa, SFo_tier_2, rice_straw, rice_straw_tier_2, yield_ref, yield_tier_2, rice_slope, rice_intercept, straw_tonnes_tier_2, methane_ef, rice_cf, nitrous_ef, nitrous_constant,
+    def __init__(self, area_start, area_end, EFc_ref, EFc_tier_2, SFw_ref, SFw_tier_2, SFp_ref, SFp_tier_2, cfoa, SFo_tier_2, rice_straw_tier_2, yield_ref, yield_tier_2, rice_slope, rice_intercept, straw_tonnes_tier_2, methane_ef, rice_cf, nitrous_ef, nitrous_constant,
                         time_impl, time_cap, rate, methane_constant, cultivation_period_ref, cultivation_period_tier_2,  socref, soc_tier_2, f_lu_ref, f_lu_tier_2, f_i_ref, f_i_tier_2, f_mg_ref, f_mg_tier_2,
                         ):
         
@@ -18,7 +18,6 @@ class FloodedRice:
         self.SFp_tier_2 = SFp_tier_2
         self.cfoa = cfoa
         self.SFo_tier_2 = SFo_tier_2
-        self.rice_straw = rice_straw
         self.rice_straw_tier_2 = rice_straw_tier_2
         self.yield_ref = yield_ref
         self.yield_tier_2 = yield_tier_2
