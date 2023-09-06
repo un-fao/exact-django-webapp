e4ce5cbd8f5e01aa511390b0374bbb1617d48a7e
Peter Anthony Wright <peter.wright95@gmail.com>
2023-09-06 11:59:49 +0200

    Fix flooded_rice
    SFo inputs replaced by cfoa:
    SFo_ref --> cfoa
    SFo_tier_2 --> cfoa_tier_2
    Also added straw_ref and straw_tier_2

--- djangoexact/math_model/no_time_dependency_final/flooded_rice.py
+++ djangoexact/math_model/no_time_dependency_final/flooded_rice.py
@@ -4,8 +4,8 @@ import traceback
 
 class FloodedRice:
 
-    def __init__(self, area_start, area_end, EFc_ref, EFc_tier_2, SFw_ref, SFw_tier_2, SFp_ref, SFp_tier_2, SFo_ref, SFo_tier_2, yield_ref, rice_slope, rice_intercept, straw_tonnes_tier_2, methane_ef, rice_cf, nitrous_ef, nitrous_constant,
-                        time_impl, time_cap, rate, methane_constant, cultivation_period_ref, cultivation_period_tier_2,  socref, soc_tier_2, f_lu_ref, f_lu_tier_2, f_i_ref, f_i_tier_2, f_mg_ref, f_mg_tier_2
+    def __init__(self, area_start, area_end, EFc_ref, EFc_tier_2, SFw_ref, SFw_tier_2, SFp_ref, SFp_tier_2, cfoa, cfoa_tier_2, rice_straw, rice_straw_tier_2, yield_ref, rice_slope, rice_intercept, straw_tonnes_tier_2, methane_ef, rice_cf, nitrous_ef, nitrous_constant,
+                        time_impl, time_cap, rate, methane_constant, cultivation_period_ref, cultivation_period_tier_2,  socref, soc_tier_2, f_lu_ref, f_lu_tier_2, f_i_ref, f_i_tier_2, f_mg_ref, f_mg_tier_2,
                         ):
         
         self.area_start = area_start
@@ -16,8 +16,10 @@ class FloodedRice:
         self.SFw_tier_2 = SFw_tier_2
         self.SFp_ref = SFp_ref
         self.SFp_tier_2 = SFp_tier_2
-        self.SFo_ref = SFo_ref
-        self.SFo_tier_2 = SFo_tier_2
+        self.cfoa = cfoa
+        self.cfoa_tier_2 = cfoa_tier_2
+        self.rice_straw = rice_straw
+        self.rice_straw_tier_2 = rice_straw_tier_2
         self.yield_ref = yield_ref
         self.rice_slope = rice_slope
         self.rice_intercept = rice_intercept
@@ -69,7 +71,9 @@ class FloodedRice:
                 EFc = self.EFc_ref if not self.EFc_tier_2 else self.EFc_tier_2
                 SFw = self.SFw_ref if not self.SFw_tier_2 else self.SFw_tier_2
                 SFp = self.SFp_ref if not self.SFp_tier_2 else self.SFp_tier_2
-                SFo = self.SFo_ref if not self.SFo_tier_2 else self.SFo_tier_2
+                cfoa = self.cfoa if not self.cfoa_tier_2 else self.cfoa_tier_2
+                rice_straw = self.rice_straw if not self.rice_straw_tier_2 else self.rice_straw_tier_2
+                SFo = 1 + rice_straw * cfoa * 0.59
 
                 if self.area_start == 0 and self.area_end == 0:
                     adjusted_daily_ef_methane_ref = 0
