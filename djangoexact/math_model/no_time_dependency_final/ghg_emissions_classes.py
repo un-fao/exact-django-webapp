77c14e0ad3adf5aef794b1cba3dabf253298d09d
Peter Anthony Wright <peter.wright95@gmail.com>
2023-11-16 11:01:01 +0100

    Add method for breakdown per gas

--- djangoexact/math_model/no_time_dependency_final/ghg_emissions_classes.py
+++ djangoexact/math_model/no_time_dependency_final/ghg_emissions_classes.py
@@ -22,18 +22,40 @@ class Emission:
         self.gas_type: GasTypes | None = gas_type
         self.value: float = value
 
-class YearlyActivityEmissionSet:
+class GasEmissionSet:
 
-    def __init__(self, year, gas_type, emissions, activity):
+    def __init__(self,year, gas_type, emissions):
         self.year: int = year
         self.gas_type: GasTypes = gas_type
         self.emissions: list[Emission] = emissions
+class YearlyActivityEmissionSet(GasEmissionSet):
+
+    def __init__(self, year, gas_type, emissions, activity):
+        super().__init__(year, gas_type, emissions)
+        # Can be a sub-activity, e.g. "Fire on Soil"
         self.activity: ActivityTypes = activity
 
 class Result:
 
-    def __init__(self):
-       
+    def __init__(self, time_impl, time_cap):
         self.yearly_emissions_by_sector_by_gas: list[YearlyActivityEmissionSet] = []
+        self.total_years = time_impl + time_cap
+
+    def breakdown_by_gas(self):
+
+        aggregated_emissions = {gas_type: GasEmissionSet(0, gas_type, [0 for i in self.total_years]) for gas_type in GasTypes}
+
+        for yearly_emission in self.yearly_emissions_by_sector_by_gas:
+            aggregated_emissions[yearly_emission.gas_type].emissions = [x + y for x,y in zip(aggregated_emissions[yearly_emission.gas_type].emissions, yearly_emission.emissions)]
+        
+        return aggregated_emissions
+    
+    def breakdown_by_activity(self):
+
+        return self.yearly_emissions_by_sector_by_gas
+    
+    
+
+        
 
     # Here add all necessary functions for result aggregation depending on what Claudio needs
