4e7a5d9675dadefb4789ecb3c67a5a5b6c115846
Peter Anthony Wright <peter.wright95@gmail.com>
2023-11-16 10:36:01 +0100

    Add Result Breakdown Grassland

--- djangoexact/math_model/no_time_dependency_final/grassland_management.py
+++ djangoexact/math_model/no_time_dependency_final/grassland_management.py
@@ -1,6 +1,7 @@
 import math
 from .general_functions import yearly_constant_emissions_breakdown, yearly_time_dependent_parameter_breakdown, yearly_time_dependent_20_year_breakdown, breakdown_according_to_values, soil_emissions
 import traceback
+from .ghg_emissions_classes import GasTypes, ActivityTypes, Emission, YearlyActivityEmissionSet, Result
 
 class GrasslandManagement:
 
@@ -50,6 +51,8 @@ class GrasslandManagement:
         self.emissions_total_yearly = []
         self.total_emissions = 0
 
+        self.result = Result()
+
         return
     
     def calculate_emissions(self,):
@@ -60,6 +63,21 @@ class GrasslandManagement:
                 if self.time_impl + self.time_cap < self.fire_interval or not self.fire_used:
                     self.emissions_residue_burning_yearly = [0 for i in range(self.time_impl + self.time_cap)]
                     self.emissions_residue_burning_total = 0
+
+                    # NOTE: needed? Or no?
+                    self.result.yearly_emissions_by_sector_by_gas.append(YearlyActivityEmissionSet(
+                        year = 0,
+                        gas_type = GasTypes.N2O,
+                        emissions = [Emission(0, GasTypes.N2O) for i in range(self.time_impl + self.time_cap)],
+                        activity = ActivityTypes.RESIDUE_BURNING
+                    ))
+
+                    self.result.yearly_emissions_by_sector_by_gas.append(YearlyActivityEmissionSet(
+                        year = 0,
+                        gas_type = GasTypes.CH4,
+                        emissions = [Emission(0, GasTypes.CH4) for i in range(self.time_impl + self.time_cap)],
+                        activity = ActivityTypes.RESIDUE_BURNING
+                    ))
                 
                 else:
                     agb = self.agb_ref if not self.agb_tier_2 else self.agb_tier_2
@@ -70,6 +88,26 @@ class GrasslandManagement:
                     total = annual_co2 * sum(self.total_hectars)
                     self.emissions_residue_burning_yearly = breakdown_according_to_values(total, self.total_hectars)
                     self.emissions_residue_burning_total = total
+
+                    annual_nitrous = (agb * cf * self.nitrous_ef * self.nitrous_constant / 1000) / self.fire_interval
+                    annual_methane = (agb * cf * self.methane_ef * self.methane_constant / 1000) / self.fire_interval
+
+                    total_nitrous = annual_nitrous * sum(self.total_hectars)
+                    total_methane = annual_methane * sum(self.total_hectars)
+
+                    self.result.yearly_emissions_by_sector_by_gas.append(YearlyActivityEmissionSet(
+                        year = 0,
+                        gas_type = GasTypes.N2O,
+                        emissions = [Emission(e, GasTypes.N2O) for e in breakdown_according_to_values(total_nitrous, self.total_hectars)],
+                        activity = ActivityTypes.RESIDUE_BURNING
+                    ))
+
+                    self.result.yearly_emissions_by_sector_by_gas.append(YearlyActivityEmissionSet(
+                        year = 0,
+                        gas_type = GasTypes.CH4,
+                        emissions = [Emission(e, GasTypes.CH4) for e in breakdown_according_to_values(total_methane, self.total_hectars)],
+                        activity = ActivityTypes.RESIDUE_BURNING
+                    ))
                 return
             except:
                 traceback.print_exc()
@@ -90,6 +128,14 @@ class GrasslandManagement:
 
                 self.emissions_soil_yearly = breakdown_according_to_values(total, self.hectars_before_20)
                 self.emissions_soil_total = total
+
+                self.result.yearly_emissions_by_sector_by_gas.append(YearlyActivityEmissionSet(
+                    year = 0,
+                    gas_type = GasTypes.CO2,
+                    emissions = [Emission(e, GasTypes.CO2) for e in breakdown_according_to_values(total, self.hectars_before_20)],
+                    activity = ActivityTypes.SOIL_CO2_CHANGE
+                ))
+                
                 return
             except:
                 traceback.print_exc()
