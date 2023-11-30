2e198a06dd9ab64a3e7b88ee401a0f06b36264e3
Claudio Lavacca <claudio.lavacca@fao.org>
2023-11-30 03:04:46 +0100

    Add m2m moduletypes to activity

--- djangoexact/api/models.py
+++ djangoexact/api/models.py
@@ -470,6 +470,8 @@ class Activity(Historical):
 
     change_rate = ForeignKey(ChangeRate, on_delete=CASCADE, related_name="activities", null=True, blank=True)
 
+    modules = ManyToManyField("api.ModuleType", related_name="activities", null=True, blank=True)
+
     def __str__(self):
         return f"({self.pk}) {self.name} in {self.project.name}"
 
