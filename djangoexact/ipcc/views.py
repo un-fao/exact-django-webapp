f8078a65ec4cc120ba6896a884c0908d5faefa7e
Claudio Lavacca <claudio.lavacca@fao.org>
2023-12-12 12:16:21 +0100

    Add dynamic filtering for ipcc endpoints

--- djangoexact/ipcc/views.py
+++ djangoexact/ipcc/views.py
@@ -3,15 +3,23 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Model
 from .serializers import get_model_serializer
+import django_filters
 
 
 class AuthenticatedViewSet:
     permission_classes = [IsAuthenticated]
 
 
-def generic_viewset(model: Model):
+
+def generic_viewset(_model: Model):
+    class GenericFilterSet(django_filters.FilterSet):
+        class Meta:
+            model = _model
+            fields = "__all__"
     class GenericViewSet(viewsets.ModelViewSet, AuthenticatedViewSet):
-        queryset = model.objects.all()
-        serializer_class = get_model_serializer(model)
+        queryset = _model.objects.all()
+        serializer_class = get_model_serializer(_model)
+        filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+        filterset_class = GenericFilterSet
 
     return GenericViewSet
