ac23dc463fd65a7cd11d021ad6b75e06ed660d6d
Claudio Lavacca <claudio.lavacca@fao.org>
2023-12-27 16:18:39 +0100

    Add instance based project permissions

--- djangoexact/api/decorators.py
+++ djangoexact/api/decorators.py
@@ -1,6 +1,9 @@
 from functools import wraps
+
 from django.http import HttpResponseForbidden
-from api.models import Project, Activity
+
+from api.models import Activity, Project, UserProjectGroup
+
 
 def can_access(resource: str):
     def decorator_func(func):
@@ -26,7 +29,33 @@ def can_access(resource: str):
 
             if can_access:
                 return func(request, *args, **kwargs)
-            
+
             return HttpResponseForbidden("You don't have permission to access this resource")
+
         return wrapper
+
     return decorator_func
+
+
+"""membership: UserProjectGroup = project.members.filter(user=self.request.user).first()
+can_view = membership and membership.group.permissions.filter(codename="view_project").exists()
+"""
+
+
+def can_access_project(func):
+    @wraps(func)
+    def wrapper(viewset, *args, **kwargs):
+        request = viewset.request
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden("User is not authenticated")
+
+        project = Project.objects.get(pk=kwargs["pk"])
+        membership: UserProjectGroup = project.members.filter(user=request.user).first()
+        can_access = membership and membership.group.permissions.filter(codename="view_project").exists()
+
+        if can_access:
+            return func(viewset, *args, **kwargs)
+
+        return HttpResponseForbidden("You don't have permission to access this resource")
+
+    return wrapper
