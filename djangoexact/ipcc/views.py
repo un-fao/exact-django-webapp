09edc53fd8da85bfe1c43451f1ce6b0107eada15
Claudio Lavacca <claudio.lavacca@fao.org>
2024-02-19 18:28:20 +0100

    Add inactive models filtering

--- djangoexact/ipcc/views.py
+++ djangoexact/ipcc/views.py
@@ -1,23 +1,38 @@
-from rest_framework import generics
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
-from django.db.models import Model
-from .serializers import get_model_serializer
 import django_filters
+from django.core.exceptions import FieldDoesNotExist
+from django.db.models import Model
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import get_model_serializer
 
 
 class AuthenticatedViewSet:
     permission_classes = [IsAuthenticated]
+
 
 def generic_viewset(_model: Model):
     class GenericFilterSet(django_filters.FilterSet):
         class Meta:
             model = _model
             fields = "__all__"
+
     class GenericViewSet(viewsets.ModelViewSet, AuthenticatedViewSet):
         queryset = _model.objects.all()
         serializer_class = get_model_serializer(_model)
         filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
         filterset_class = GenericFilterSet
+
+        def get_queryset(self):
+
+            # If list operation, filter out inactive objects
+            if self.action == "list":
+                try:
+                    is_active_field = self.queryset.model._meta.get_field("is_active")
+                    return self.queryset.filter(is_active=True)
+                except FieldDoesNotExist:
+                    return super().get_queryset()
+
+            return super().get_queryset()
 
     return GenericViewSet
