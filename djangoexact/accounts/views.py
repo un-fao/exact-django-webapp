db5a8e3a621b16359765bfca899c6d48598f8a9f
Claudio Lavacca <claudio.lavacca@fao.org>
2024-02-27 14:35:56 +0100

    Add inactive user by default

--- djangoexact/accounts/views.py
+++ djangoexact/accounts/views.py
@@ -16,7 +16,11 @@ class RegisterView(generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.save()
+
+        user: User = serializer.save()
+        user.is_active = False
+        user.save()
+
         return Response(
             {
                 "user": UserSerializer(user, context=self.get_serializer_context()).data,
