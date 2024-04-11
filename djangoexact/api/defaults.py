8c3d35d2d816d2dbfab22fca2143fb747f339646
Claudio Lavacca <claudio.lavacca@fao.org>
2024-04-11 15:21:55 +0200

    Add annual cropping defaults

--- djangoexact/api/defaults.py
+++ djangoexact/api/defaults.py
@@ -2,6 +2,7 @@ from types import SimpleNamespace
 
 import ipcc.models as ipcc
 
+import api.calculators as calcs
 import api.models as api
 import api.serializers as serializers
 import api.utilities as utils
@@ -14,6 +15,12 @@ class Defaults:
         self.activity = input.parent.activity if getattr(input, "parent", None) else input.activity
         self.climate: api.Climate = self.activity.climate_t2 if self.activity.climate_t2 else self.activity.project.climate
         self.moisture: api.Moisture = self.activity.moisture_t2 if self.activity.moisture_t2 else self.activity.project.moisture
+        self.soil_type: api.SoilType = self.activity.project.soil_type
+
+        self.module_start = self.module_w = self.module_wo = self.input
+        luc = getattr(self.input, "land_use_change", None)
+        if luc:
+            self.module_start, self.module_w, self.module_wo = calcs.get_luc_modules(luc)
 
     def get_defaults() -> dict:
         """
@@ -35,6 +42,8 @@ class DefaultsFactory:
 
         if isinstance(input, api.Grassland):
             return GrasslandDefaults(input).get_defaults()
+        elif isinstance(input, api.AnnualCropping):
+            return AnnualCroppingDefaults(input).get_defaults()
         else:
             try:
                 getattr(api, input.__class__.__name__)
@@ -45,9 +54,6 @@ class DefaultsFactory:
 
 
 class GrasslandDefaults(Defaults):
-
-    def __init__(self, input: api.Grassland):
-        super().__init__(input)
 
     def get_defaults(self) -> dict:
         """
@@ -115,4 +121,154 @@ class GrasslandDefaults(Defaults):
             combustion_factor_t2_start_default=combustion_factor_t2_start_default,
             combustion_factor_t2_w_default=combustion_factor_t2_w_default,
             combustion_factor_t2_wo_default=combustion_factor_t2_wo_default,
+        )
+
+
+class AnnualCroppingDefaults(Defaults):
+    pass
+
+    def get_defaults(self) -> dict:
+        self.input: api.AnnualCropping
+
+        soc_t2_start_default = 0
+        soc_t2_w_default = 0
+        soc_t2_wo_default = 0
+
+        fi_t2_start_default = 0
+        fi_t2_w_default = 0
+        fi_t2_wo_default = 0
+
+        fmg_t2_start_default = 0
+        fmg_t2_w_default = 0
+        fmg_t2_wo_default = 0
+
+        flu_t2_start_default = 0
+        flu_t2_w_default = 0
+        flu_t2_wo_default = 0
+
+        burning_emission_factor_t2_start_default = 0
+        burning_emission_factor_t2_w_default = 0
+        burning_emission_factor_t2_wo_default = 0
+
+        defaults = calcs.AnnualCropCalculator(self.input)
+        defaults.get_defaults()
+
+        soc_t2_start_default = defaults.soc.value
+        soc_t2_w_default = defaults.soc.value
+        soc_t2_wo_default = defaults.soc.value
+
+        fi_t2_start_default = defaults.fi_start.value
+        fi_t2_w_default = defaults.fi_w.value
+        fi_t2_wo_default = defaults.fi_wo.value
+
+        fmg_t2_start_default = defaults.fmg_start.value
+        fmg_t2_w_default = defaults.fmg_w.value
+        fmg_t2_wo_default = defaults.fmg_wo.value
+
+        flu_t2_start_default = defaults.flu_start.value
+        flu_t2_w_default = defaults.flu_w.value
+        flu_t2_wo_default = defaults.flu_wo.value
+
+        burning_emission_factor_t2_start_default = defaults.fires_start.value
+        burning_emission_factor_t2_w_default = defaults.fires_w.value
+        burning_emission_factor_t2_wo_default = defaults.fires_wo.value
+
+        # # TODO: Move this to staticmethod of calculators, maybe?
+        # try:
+        #     soc = ipcc.SoilOrganicCarbon.objects.get(climate=self.climate, moisture=self.moisture, soil_type=self.soil_type).value
+        # except ipcc.SoilOrganicCarbon.DoesNotExist:
+        #     raise ValueError(f"Soil organic carbon default value not found for climate {self.climate}, moisture {self.moisture} and soil type {self.soil_type}")
+
+        # try:
+        #     fi_t2_start_default = calcs.get_fi_data(self.module_start, self.climate, self.moisture, utils.ScenarioTypes.START)
+        # except ipcc.FIData.DoesNotExist:
+        #     raise ValueError("Annual cropping default FI start value not found.")
+
+        # try:
+        #     fi_t2_w_default = calcs.get_fi_data(self.module_w, self.climate, self.moisture, utils.ScenarioTypes.WITH)
+        # except ipcc.FIData.DoesNotExist:
+        #     raise ValueError("Annual cropping default FI with value not found.")
+
+        # try:
+        #     fi_t2_wo_default = calcs.get_fi_data(self.module_wo, self.climate, self.moisture, utils.ScenarioTypes.WITHOUT)
+        # except ipcc.FIData.DoesNotExist:
+        #     raise ValueError("Annual cropping default FI without value not found.")
+
+        # try:
+        #     fmg_t2_start_default = calcs.get_fmg_data(self.module_start, self.climate, self.moisture, utils.ScenarioTypes.START)
+        # except ipcc.FMGData.DoesNotExist:
+        #     raise ValueError("Annual cropping default FMG start value not found.")
+
+        # try:
+        #     fmg_t2_w_default = calcs.get_fmg_data(self.module_w, self.climate, self.moisture, utils.ScenarioTypes.WITH)
+        # except ipcc.FMGData.DoesNotExist:
+        #     raise ValueError("Annual cropping default FMG with value not found.")
+
+        # try:
+        #     fmg_t2_wo_default = calcs.get_fmg_data(self.module_wo, self.climate, self.moisture, utils.ScenarioTypes.WITHOUT)
+        # except ipcc.FMGData.DoesNotExist:
+        #     raise ValueError("Annual cropping default FMG without value not found.")
+
+        # try:
+        #     flu_t2_start_default = calcs.get_flu_data(self.module_start, self.climate, self.moisture, utils.ScenarioTypes.START)
+        # except ipcc.FLUData.DoesNotExist:
+        #     raise ValueError("Annual cropping default FLU start value not found.")
+
+        # try:
+        #     flu_t2_w_default = calcs.get_flu_data(self.module_w, self.climate, self.moisture, utils.ScenarioTypes.WITH)
+        # except ipcc.FLUData.DoesNotExist:
+        #     raise ValueError("Annual cropping default FLU with value not found.")
+
+        # try:
+        #     flu_t2_wo_default = calcs.get_flu_data(self.module_wo, self.climate, self.moisture, utils.ScenarioTypes.WITHOUT)
+        # except ipcc.FLUData.DoesNotExist:
+        #     raise ValueError("Annual cropping default FLU without value not found.")
+
+        # try:
+        #     burning_emission_factor = ipcc.FiresCombustionFactor.objects.get(land_use_type=self.input.land_use_type_start).value
+        # except ipcc.FiresCombustionFactor.DoesNotExist:
+        #     raise ValueError(f"Annual cropping default burning emission factor not found for land use type {self.input.land_use_type_start}")
+
+        # soc_t2_start_default = soc
+        # soc_t2_w_default = soc
+        # soc_t2_wo_default = soc
+
+        # burning_emission_factor_t2_start_default = burning_emission_factor
+        # burning_emission_factor_t2_w_default = burning_emission_factor
+        # burning_emission_factor_t2_wo_default = burning_emission_factor
+
+        # return SimpleNamespace(
+        #     soc_t2_start_default=soc_t2_start_default,
+        #     soc_t2_w_default=soc_t2_w_default,
+        #     soc_t2_wo_default=soc_t2_wo_default,
+        #     fi_t2_start_default=fi_t2_start_default,
+        #     fi_t2_w_default=fi_t2_w_default,
+        #     fi_t2_wo_default=fi_t2_wo_default,
+        #     fmg_t2_start_default=fmg_t2_start_default,
+        #     fmg_t2_w_default=fmg_t2_w_default,
+        #     fmg_t2_wo_default=fmg_t2_wo_default,
+        #     flu_t2_start_default=flu_t2_start_default,
+        #     flu_t2_w_default=flu_t2_w_default,
+        #     flu_t2_wo_default=flu_t2_wo_default,
+        #     burning_emission_factor_t2_start_default=burning_emission_factor_t2_start_default,
+        #     burning_emission_factor_t2_w_default=burning_emission_factor_t2_w_default,
+        #     burning_emission_factor_t2_wo_default=burning_emission_factor_t2_wo_default,
+        # )
+
+        return SimpleNamespace(
+            soc_t2_start_default=soc_t2_start_default,
+            soc_t2_w_default=soc_t2_w_default,
+            soc_t2_wo_default=soc_t2_wo_default,
+            fi_t2_start_default=fi_t2_start_default,
+            fi_t2_w_default=fi_t2_w_default,
+            fi_t2_wo_default=fi_t2_wo_default,
+            fmg_t2_start_default=fmg_t2_start_default,
+            fmg_t2_w_default=fmg_t2_w_default,
+            fmg_t2_wo_default=fmg_t2_wo_default,
+            flu_t2_start_default=flu_t2_start_default,
+            flu_t2_w_default=flu_t2_w_default,
+            flu_t2_wo_default=flu_t2_wo_default,
+            burning_emission_factor_t2_start_default=burning_emission_factor_t2_start_default,
+            burning_emission_factor_t2_w_default=burning_emission_factor_t2_w_default,
+            burning_emission_factor_t2_wo_default=burning_emission_factor_t2_wo_default,
         )
