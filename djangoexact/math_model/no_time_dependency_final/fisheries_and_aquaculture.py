4d1d14e1fe0d6f0be792a16eba59a355cac60e8f
peterwright95 <peter.wright95@gmail.com>
2024-05-02 11:54:06 +0200

    Fix aquacultre

--- djangoexact/math_model/no_time_dependency_final/fisheries_and_aquaculture.py
+++ djangoexact/math_model/no_time_dependency_final/fisheries_and_aquaculture.py
@@ -236,7 +236,7 @@ class CoastalAquaculture:
         calculate_co2_emissions()
 
         try:
-            self.emissions_total_yearly = [sum(x + y) for x, y in zip(self.emissions_nitrous_yearly, self.emissions_co2_yearly)]
+            self.emissions_total_yearly = [sum(x) for x in zip(self.emissions_nitrous_yearly, self.emissions_co2_yearly)]
             self.total_emissions = sum(self.emissions_total_yearly)
         except Exception as e:
             traceback.print_exc()
