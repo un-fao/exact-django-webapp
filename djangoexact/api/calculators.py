92de46d6103ad37905ea2dc03a8887f00a61525f
peterwright95 <peter.wright95@gmail.com>
2024-06-12 13:04:44 +0200

    Add SmallFishery Tier2 Defaults

--- djangoexact/api/calculators.py
+++ djangoexact/api/calculators.py
@@ -2550,8 +2550,34 @@ class SmallFisheryCalculator(BaseCalculator):
     Calculator for small fishery.
     """
 
-    def get_defaults(self, input: Module) -> dict:
-        return super().get_defaults(input)
+    def get_defaults(self, calculate = False) -> dict:
+
+        module: SmallFishery = self.data
+        activity: Activity = module.activity
+        project: Project = activity.project
+
+        try:
+            ef_diesel_default_list = ipcc.EnergyDefaultEmissionFactor.objects.filter(fuel_type__fuel_use_type__name__contains="Off-Road")
+            # Average of all default emission factors for gasoil/diesel
+            self.ef_diesel_default = sum([ef.t_co2_eq for ef in ef_diesel_default_list]) / len(ef_diesel_default_list)
+        except ipcc.EnergyDefaultEmissionFactor.DoesNotExist:
+            raise ValueError("Default emission factors for off-road diesel do not exist")
+
+        try:
+            self.lost_refrigerant_default = SmallFisheryParameter.objects.get(name="lost_refrigerant_default").value
+        except SmallFisheryParameter.DoesNotExist:
+            raise ValueError("Default lost refrigerant does not exist")
+
+        try:
+            self.tonnes_ice_default = SmallFisheryParameter.objects.get(name="tonnes_ice_default").value
+        except SmallFisheryParameter.DoesNotExist:
+            raise ValueError("Default tonnes of ice does not exist")
+
+        try:
+            self.kw_tonnes = SmallFisheryParameter.objects.get(name="kw_tonnes").value
+        except SmallFisheryParameter.DoesNotExist:
+            raise ValueError("Default kw per tonne does not exist")
+
 
     def calculate(self) -> list[Result]:
         """
