c5b4f87bbafaa6766f961724dbda280fc9b21972
Claudio Lavacca <claudio.lavacca@fao.org>
2024-07-15 19:28:24 +0200

    Cut redundant middleware
    Add password reset endpoint

--- djangoexact/accounts/utils.py
+++ djangoexact/accounts/utils.py
@@ -6,13 +6,14 @@ from accounts.firebase import firebase_admin_auth
 
 # Create custom email verification link
 def send_email_verification_link(user_email, display_name):
-    action_code_settings = {
-        firebase_admin_auth.ActionCodeSettings(
-            url="https://exact.fao.org/",
-            handle_code_in_app=True,
-        )
-    }
     custom_email_link = firebase_admin_auth.generate_email_verification_link(user_email)
     subject = "Verify your email address"
     message = f"Hi {display_name},\n\nPlease verify your email address by clicking on the link below:\n\n{custom_email_link}"
+    send_mail(subject, message, settings.EMAIL_HOST_USER, [user_email], fail_silently=False)
+
+
+def send_password_reset_link(user_email):
+    custom_email_link = firebase_admin_auth.generate_password_reset_link(user_email)
+    subject = "Reset your password"
+    message = f"Hi,\n\nPlease reset your password by clicking on the link below:\n\n{custom_email_link}"
     send_mail(subject, message, settings.EMAIL_HOST_USER, [user_email], fail_silently=False)
--- djangoexact/api/models.py
+++ djangoexact/api/models.py
@@ -61,7 +61,7 @@ class CustomUser(AbstractUser):
     country = models.ForeignKey("api.Country", on_delete=models.CASCADE, null=True, blank=True, related_name="users")
     email = models.EmailField(unique=True)
     username = None
-    firebase_uid = models.CharField(max_length=255, unique=True, validators=[alphanumeric], null=True, verbose_name="UID")
+    firebase_uid = models.CharField(max_length=255, unique=True, validators=[alphanumeric], null=True, blank=True, verbose_name="Firebase UID")
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
