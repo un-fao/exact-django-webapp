9f0eb73df19e60d976baf1ea2e8fc4f0a4efe6f4
Claudio Lavacca <claudio.lavacca@fao.org>
2024-07-18 10:41:30 +0200

    Refactor module defaults endpoint flow

--- djangoexact/api/views.py
+++ djangoexact/api/views.py
@@ -1149,12 +1149,12 @@ def generic_module_viewset(model: Model):
                 module: Module = get_object_or_404(model, pk=pk)
                 activity = module.activity
 
-            if not module.is_ready():
-                return utils.ErrorResponse("Module is not ready. Cannot calculate defaults.")
-
             if not utils.has_project_permission("can_view_modules", self.request.user, activity.project):
                 logging.error("Selected user does not have permission to view this module in the project")
                 return utils.ErrorResponse("Selected user does not have permission to view this module in the project", status=http_status.HTTP_403_FORBIDDEN)
+
+            if not module.is_ready():
+                return utils.ErrorResponse("Module is not ready. Cannot calculate defaults.")
 
             try:
                 defaults: SimpleNamespace = DefaultsFactory.get_defaults(module, calculate=True)
