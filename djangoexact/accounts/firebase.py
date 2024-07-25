27d32e2503b2c51598f2a4a2f487b84cb13c13bb
Claudio Lavacca <claudio.lavacca@fao.org>
2024-07-25 19:55:35 +0200

    Refactor firebase connection

--- djangoexact/accounts/firebase.py
+++ djangoexact/accounts/firebase.py
@@ -10,8 +10,21 @@ from rest_framework import authentication, exceptions
 from djangoexact.settings import auth
 
 try:
-    f = json.loads(base64.b64decode(os.getenv("FIREBASE_SERVICE_ACCOUNT")).decode("utf-8"))
-    cred = credentials.Certificate(f)
+    FIREBASE_CONFIG = {
+        "type": "service_account",
+        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.getenv("FIREBASE_PUBLIC_KEY"),
+        "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+        "client_email": os.getenv("FIREBASE_SERVICE_EMAIL"),
+        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.getenv("FIREBASE_CERT_URL"),
+        "universe_domain": "googleapis.com",
+    }
+
+    cred = credentials.Certificate(FIREBASE_CONFIG)
     auth = firebase_admin.initialize_app(cred)
 except Exception as e:
     raise Exception(f"Firebase config not found: {e}") from e
