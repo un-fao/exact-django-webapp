0cf188baaffd5b50c9b779978f9b33816caa6a18
Claudio Lavacca <claudio.lavacca@fao.org>
2024-07-26 13:10:46 +0200

    Fix firebase settings for review

--- djangoexact/djangoexact/settings.py
+++ djangoexact/djangoexact/settings.py
@@ -220,15 +220,15 @@ AUTH_USER_MODEL = "api.CustomUser"
 # Firebase settings
 try:
     FIREBASE_CONFIG = {
-        "apiKey": os.getenv("FIREBASE_API_KEY"),
-        "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
-        "projectId": os.getenv("FIREBASE_PROJECT_ID"),
-        "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
-        "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
-        "appId": os.getenv("FIREBASE_APP_ID"),
-        "measurementId": os.getenv("FIREBASE_MEASUREMENT_ID"),
+        "apiKey": os.getenv("FIREBASE_API_KEY", "$FIREBASE_API_KEY"),
+        "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN", "$FIREBASE_AUTH_DOMAIN"),
+        "projectId": os.getenv("FIREBASE_PROJECT_ID", "$FIREBASE_PROJECT_ID"),
+        "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET", "$FIREBASE_STORAGE_BUCKET"),
+        "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID", "$FIREBASE_MESSAGING_SENDER_ID"),
+        "appId": os.getenv("FIREBASE_APP_ID", "$FIREBASE_APP_ID"),
+        "measurementId": os.getenv("FIREBASE_MEASUREMENT_ID", "$FIREBASE_MEASUREMENT_ID"),
         "databaseURL": "",
-        "serviceAccount": json.loads(base64.b64decode(os.getenv("FIREBASE_SERVICE_ACCOUNT")).decode()),
+        "serviceAccount": json.loads(base64.b64decode(os.getenv("FIREBASE_SERVICE_ACCOUNT", "$FIREBASE_SERVICE_ACCOUNT")).decode()),
     }
 
     print(FIREBASE_CONFIG)
