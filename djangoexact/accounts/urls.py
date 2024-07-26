99cdf5274d4e48bbc564ac1c5d4e4aa3a1c27bdb
Claudio Lavacca <claudio.lavacca@fao.org>
2024-07-26 17:39:06 +0200

    Cut firebase transfer endpoint

--- djangoexact/accounts/urls.py
+++ djangoexact/accounts/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path("password-reset/", views.PasswordResetView.as_view(), name="password_reset"),
     path("token/refresh/", views.TokenRefreshView.as_view(), name="token_refresh"),
     path("verify/", views.VerifyUserEmail.as_view(), name="verify_user_email"),
-    path("transfer/", views.TransferUser.as_view(), name="transfer_user"),
 ]
