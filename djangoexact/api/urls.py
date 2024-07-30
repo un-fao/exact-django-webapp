8741485a8f8b5962b0cfb6640038d90eb4369944
Claudio Lavacca <claudio.lavacca@fao.org>
2024-07-30 10:51:05 +0200

    Cut authentication for swagger

--- djangoexact/api/urls.py
+++ djangoexact/api/urls.py
@@ -19,7 +19,6 @@ schema_view = get_schema_view(
         # license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=[permissions.IsAuthenticated],
 )
 
 router = routers.DefaultRouter()
