047b7617de01afa810f897fd32746ad13e3eebcf
Claudio Lavacca <claudio.lavacca@fao.org>
2024-08-12 11:23:09 +0200

    Add django unfold library

--- djangoexact/api/admin.py
+++ djangoexact/api/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
 from .models import *
 
 for model in [model for model in dir() if not model.startswith("_")]:
     try:
-        admin.site.register(eval(model))
+        admin.site.register(eval(model), ModelAdmin)
     except:
         pass
