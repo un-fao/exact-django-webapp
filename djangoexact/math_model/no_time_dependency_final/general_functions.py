6ae13c49dadf0085ab880ba95424c28cceb0534d
peterwright95 <peter.wright95@gmail.com>
2024-08-16 16:36:18 +0200

    Remove BaseModule from general_functions

--- djangoexact/math_model/no_time_dependency_final/general_functions.py
+++ djangoexact/math_model/no_time_dependency_final/general_functions.py
@@ -2,7 +2,6 @@
 import math
 import re
 import traceback
-from abc import ABC
 from dataclasses import dataclass
 
 import numpy as np
@@ -490,17 +489,3 @@ class Tier2Defaults:
     other: dict
 
 
-class BaseModule(ABC):
-    def evaluate_tier_2_defaults(self):
-        try:
-            # TODO: evaluate tier 2 defaults based on the front-end necessities
-
-            t2_start = {re.sub("_start_tier_2_default", "", k): v for k, v in self.__dict__.items() if "start_tier_2_default" in k}
-            t2_end = {re.sub("_end_tier_2_default", "", k): v for k, v in self.__dict__.items() if "end_tier_2_default" in k}
-            t2_other = {re.sub("_tier_2_default", "", k): v for k, v in self.__dict__.items() if "_tier_2_default" in k and "start" not in k and "end" not in k}
-
-            return Tier2Defaults(t2_start, t2_end, t2_other)
-
-        except Exception as e:
-            traceback.print_exc()
-            return {}
