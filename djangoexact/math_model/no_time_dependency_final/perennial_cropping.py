e25998ba43a276b7e60d812578efad72df108bee
peterwright95 <peter.wright95@gmail.com>
2024-08-19 14:58:06 +0200

    Fix perennial biomass

--- djangoexact/math_model/no_time_dependency_final/perennial_cropping.py
+++ djangoexact/math_model/no_time_dependency_final/perennial_cropping.py
@@ -215,7 +215,7 @@ class PerennialCropping(BaseModule):
             try:
                 if self.biomass_start and self.biomass_end:
                     # NOTE: This means that we have received values for both (or we have tier 2 values for both)
-                    self.emissions_biomass_yearly, self.emissions_biomass_total = biomass_emissions(self.biomass_end, self.biomass_start, self.total_hectars, self.area_start, self.area_end, self.hectars_before_20)
+                    self.emissions_biomass_yearly, self.emissions_biomass_total = biomass_emissions(self.biomass_end, self.biomass_start, self.area_start, self.area_end, self.rate, self.time_impl, self.time_cap)
 
                     biomass_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.CO2, [Emission(e, GasTypes.CO2) for e in self.emissions_biomass_yearly], ActivityTypes.BIOMASS, delay=self.delay)
                     self.result.yearly_emissions_by_sector_by_gas.append(biomass_emission_set)
@@ -241,7 +241,7 @@ class PerennialCropping(BaseModule):
 
 
                     # NOTE: maybe this should be broken down over max_years_growth or over all years of project depending on whether calculated or tabular is used
-                    self.yearly_bio_emissions = breakdown_according_to_values_for_x_years(total, self.total_hectars)
+                    self.yearly_bio_emissions = breakdown_according_to_values_for_x_years(total, self.total_hectars, len(self.total_hectars))
                     self.total_bio_emissions = total
 
                     biomass_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.CO2, [Emission(e, GasTypes.CO2) for e in self.yearly_bio_emissions], ActivityTypes.BIOMASS, delay=self.delay)
