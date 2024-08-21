df01b87227aa2f997da521b60b55a94595bfdd52
Claudio Lavacca <claudio.lavacca@fao.org>
2024-08-21 12:42:02 +0200

    Refactor test classes

--- djangoexact/api/tests/grassland_to_perennial.py
+++ djangoexact/api/tests/grassland_to_perennial.py
@@ -1,58 +1,31 @@
-import json
-import math
-from time import sleep
+import logging as log
 
-import factory.fuzzy as fuzzy
-import openpyxl as xl
-import xlwings as xw
 from api.calculators import *
 from api.models import *
-from api.models import CustomUser as User
 from api.serializers import *
-from django.test import TestCase
 from ipcc.models import *
-from openpyxl import load_workbook
-from rest_framework.test import APIRequestFactory
 
 from .factories import *
-import datetime
-
-climates = Climate.objects.all()
-countries = Country.objects.all()
-soil_types = SoilType.objects.all().exclude(active=False).exclude(name="Mineral").exclude(name="Organic")
-gw_potentials = GlobalWarmingPotential.objects.all()
-
-u = User.objects.get(email="admin@admin.com")
-
-date = datetime.datetime.now().strftime("%d%m%Y%H%M%S")
-
-climate = fuzzy.FuzzyChoice(climates).fuzz()
-moisture = fuzzy.FuzzyChoice(climate.moistures.all()).fuzz()
-
-p: ProjectFactory = ProjectFactory.create(owner=u, name=f"Grassland->Perennial {date}", climate=climate, moisture=moisture)
-
-a: Activity = ActivityFactory.create(project=p, name="Activity 1")
-
-a.module_types.add(ModuleType.objects.get(class_name="LandUseChange"))
-a.module_types.add(ModuleType.objects.get(class_name="Grassland"))
-a.module_types.add(ModuleType.objects.get(class_name="PerennialCropping"))
-a.save()
+import api.tests.base_test_classes as t
+from factory import fuzzy
 
 
-luc: LandUseChange = LandUseChangeFactory.create(
-    activity=a,
-    module_type_start=ModuleType.objects.get(class_name="Grassland"),
-    module_type_wo=ModuleType.objects.get(class_name="Grassland"),
-    module_type_w=ModuleType.objects.get(class_name="PerennialCropping"),
-)
+class GrasslandToPerenial(t.LandUseChangeTest):
+    def __init__(self):
+        super().__init__()
+        self.module_type_start = ModuleType.objects.get(class_name="Grassland")
+        self.module_type_wo = ModuleType.objects.get(class_name="Grassland")
+        self.module_type_w = ModuleType.objects.get(class_name="PerennialCropping")
+        self.create_land_use_change(self.module_type_start, self.module_type_wo, self.module_type_w)
+        self.add_activity_modules([self.module_type_start, self.module_type_wo, self.module_type_w])
 
-trees: list[LandUseType] = LandUseType.objects.filter(climates=climate, moistures=moisture, module_types=ModuleType.objects.get(class_name="PerennialCropping")).all()
+        self.trees = LandUseType.objects.filter(climates=self.climate, moistures=self.moisture, module_types=self.module_type_w).all()
 
-grassland: Grassland = GrasslandFactory.create(activity=a, land_use_change=luc)
-perennial: PerennialCropping = PerennialCroppingFactory.create(activity=a, land_use_change=luc, land_use_type_start=fuzzy.FuzzyChoice(trees).fuzz(), land_use_type_wo=fuzzy.FuzzyChoice(trees).fuzz(), land_use_type_w=fuzzy.FuzzyChoice(trees).fuzz())
+        self.module_start: Grassland = self.create_module(self.module_type_start, land_use_change=self.land_use_change)
+        self.module_end: PerennialCropping = self.create_module(self.module_type_w, land_use_change=self.land_use_change, land_use_type_start=fuzzy.FuzzyChoice(self.trees).fuzz(), land_use_type_wo=fuzzy.FuzzyChoice(self.trees).fuzz(), land_use_type_w=fuzzy.FuzzyChoice(self.trees).fuzz())
 
-grassland_results = CalculatorFactory().calculate_result(grassland)
-perennial_results = CalculatorFactory().calculate_result(perennial)
+    def test(self):
+        self.calculate_results()
 
 
-print("AO")
+GrasslandToPerenial().test()
--- djangoexact/api/tests/perennial_to_rice.py
+++ djangoexact/api/tests/perennial_to_rice.py
@@ -1,61 +1,37 @@
-import json
-import math
-from time import sleep
+import logging as log
 
-import factory.fuzzy as fuzzy
-import openpyxl as xl
-import xlwings as xw
 from api.calculators import *
 from api.models import *
-from api.models import CustomUser as User
 from api.serializers import *
-from django.test import TestCase
 from ipcc.models import *
-from openpyxl import load_workbook
-from rest_framework.test import APIRequestFactory
 
 from .factories import *
-import datetime
-
-climates = Climate.objects.all()
-countries = Country.objects.all()
-soil_types = SoilType.objects.all().exclude(active=False).exclude(name="Mineral").exclude(name="Organic")
-gw_potentials = GlobalWarmingPotential.objects.all()
-
-u = User.objects.get(email="admin@admin.com")
-
-date = datetime.datetime.now().strftime("%d%m%Y%H%M%S")
-
-climate = fuzzy.FuzzyChoice(climates).fuzz()
-moisture = fuzzy.FuzzyChoice(climate.moistures.all()).fuzz()
-
-p: ProjectFactory = ProjectFactory.create(owner=u, name=f"Perennial->Rice {date}", climate=climate, moisture=moisture)
-
-a: Activity = ActivityFactory.create(project=p, name="Activity 1")
-
-a.module_types.add(ModuleType.objects.get(class_name="LandUseChange"))
-a.module_types.add(ModuleType.objects.get(class_name="PerennialCropping"))
-a.module_types.add(ModuleType.objects.get(class_name="FloodedRice"))
-a.save()
+import api.tests.base_test_classes as t
+from factory import fuzzy
 
 
-luc: LandUseChange = LandUseChangeFactory.create(
-    activity=a,
-    module_type_start=ModuleType.objects.get(class_name="PerennialCropping"),
-    module_type_wo=ModuleType.objects.get(class_name="PerennialCropping"),
-    module_type_w=ModuleType.objects.get(class_name="FloodedRice"),
-)
+class PerennialToRice(t.LandUseChangeTest):
+    def __init__(self):
+        super().__init__()
+        self.module_type_start = ModuleType.objects.get(class_name="PerennialCropping")
+        self.module_type_wo = ModuleType.objects.get(class_name="PerennialCropping")
+        self.module_type_w = ModuleType.objects.get(class_name="FloodedRice")
+        self.create_land_use_change(self.module_type_start, self.module_type_wo, self.module_type_w)
+        self.add_activity_modules([self.module_type_start, self.module_type_wo, self.module_type_w])
 
-perennial_lut_start: LandUseType = fuzzy.FuzzyChoice(LandUseType.objects.filter(climates=climate, moistures=moisture, module_types=ModuleType.objects.get(class_name="PerennialCropping")).all()).fuzz()
-perennial_lut_w: LandUseType = fuzzy.FuzzyChoice(LandUseType.objects.filter(climates=climate, moistures=moisture, module_types=ModuleType.objects.get(class_name="PerennialCropping")).all()).fuzz()
-perennial_lut_wo: LandUseType = fuzzy.FuzzyChoice(LandUseType.objects.filter(climates=climate, moistures=moisture, module_types=ModuleType.objects.get(class_name="PerennialCropping")).all()).fuzz()
+        self.trees = LandUseType.objects.filter(climates=self.climate, moistures=self.moisture, module_types=self.module_type_start).all()
 
-perennial: PerennialCropping = PerennialCroppingFactory.create(activity=a, land_use_change=luc, land_use_type_start=perennial_lut_start, land_use_type_wo=perennial_lut_wo, land_use_type_w=perennial_lut_w)
-flooded_rice: FloodedRice = FloodedRiceFactory.create(activity=a, land_use_change=luc)
+        self.module_start: PerennialCropping = self.create_module(
+            self.module_type_start,
+            land_use_change=self.land_use_change,
+            land_use_type_start=fuzzy.FuzzyChoice(self.trees).fuzz(),
+            land_use_type_wo=fuzzy.FuzzyChoice(self.trees).fuzz(),
+            land_use_type_w=fuzzy.FuzzyChoice(self.trees).fuzz(),
+        )
+        self.module_end: FloodedRice = self.create_module(self.module_type_w, land_use_change=self.land_use_change)
+
+    def test(self):
+        self.calculate_results()
 
 
-perennial_results = CalculatorFactory().calculate_result(perennial)
-flooded_rice_results = CalculatorFactory().calculate_result(flooded_rice)
-
-
-print("AO")
+PerennialToRice().test()
