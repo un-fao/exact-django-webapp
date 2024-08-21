2844292945742ce0cfc964f78bb66afb033b2a13
Claudio Lavacca <claudio.lavacca@fao.org>
2024-08-21 14:43:16 +0200

    Add more independent modules test classes

---
+++ djangoexact/api/tests/modules/input.py
@@ -0,0 +1,27 @@
+import logging as log
+
+from api.calculators import *
+from api.models import *
+from api.serializers import *
+from ipcc.models import *
+
+from ..factories import *
+import api.tests.base_test_classes as t
+
+
+class InputTest(t.ModuleWithSubmodulesTest):
+    def __init__(self):
+        super().__init__()
+        self.module_type = ModuleType.objects.get(class_name="Input")
+        self.submodule_types = [
+            ModuleType.objects.get(class_name="InputEntry"),
+        ]
+        self.create_module()
+        self.create_submodules()
+
+    def test(self):
+        self.calculate_results()
+        self.calculate_submodule_results()
+
+
+InputTest().test()
