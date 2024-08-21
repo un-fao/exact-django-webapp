9ae1ef94686a58e4959a6fc527ec5cc4556a4ad8
peterwright95 <peter.wright95@gmail.com>
2024-08-21 11:59:55 +0200

    Update Perennial to dataclass

--- djangoexact/math_model/no_time_dependency_final/perennial_cropping.py
+++ djangoexact/math_model/no_time_dependency_final/perennial_cropping.py
@@ -1,7 +1,6 @@
 import traceback
 
 from .general_functions import (
-    BaseModule,
     breakdown_according_to_values,
     soil_emissions_2,
     som_emissions,
@@ -19,145 +18,29 @@ from .ghg_emissions_classes import (
     YearlyGasActivityEmissionSet,
 )
 
+from .generalized_modules import LandModule
+from typing import Optional
+from dataclasses import dataclass
 
-class PerennialCropping(BaseModule):
-    def __init__(
-        self,
-        area_start,
-        area_end,
-        time_impl,
-        time_cap,
-        rate,
-        nitrous_constant,
-        methane_constant,
-        residue_burnt,
-        emission_factor_burning_nitrous_residue,
-        ef_nitrous_som,
-        emission_factor_burning_methane,
-        combustion_factor,
-        fire_periodicity_default,
-        fire_periodicity_tier_2,
-        t_biomass_tier_2,
-        agb_rate_default,
-        agb_rate_tier_2,
-        agb_maximum_c,
-        bgb_rate_default,
-        bgb_rate_tier_2,
-        soc_start_default,
-        soc_end_default,
-        soc_start_tier_2,
-        soc_end_tier_2,
-        fmg_start_default,
-        fmg_end_default,
-        fmg_start_tier_2,
-        fmg_end_tier_2,
-        flu_start_default,
-        flu_end_default,
-        flu_start_tier_2,
-        flu_end_tier_2,
-        fi_start_default,
-        fi_end_default,
-        fi_start_tier_2,
-        fi_end_tier_2,
-        calculate_soc_som,
-        delay,
-        biomass_start_default,
-        biomass_end_default,
-        biomass_start_tier_2,
-        biomass_end_tier_2,
-    ):
-        self.area_start = area_start
-        self.area_end = area_end
-        self.time_impl = time_impl
-        self.time_cap = time_cap
-        self.rate = rate
-        self.nitrous_constant = nitrous_constant
-        self.methane_constant = methane_constant
-        self.residue_burnt = residue_burnt
-        self.emission_factor_burning_nitrous_residue = emission_factor_burning_nitrous_residue
-        self.ef_nitrous_som = ef_nitrous_som
-        self.emission_factor_burning_methane = emission_factor_burning_methane
-        self.combustion_factor = combustion_factor
-        self.fire_periodicity_default = fire_periodicity_default
-        self.fire_periodicity_tier_2 = fire_periodicity_tier_2
-        self.t_biomass_tier_2 = t_biomass_tier_2
-        self.agb_rate_default = agb_rate_default
-        self.agb_rate_tier_2 = agb_rate_tier_2
-        self.agb_maximum_c = agb_maximum_c
-        self.bgb_rate_default = bgb_rate_default
-        self.bgb_rate_tier_2 = bgb_rate_tier_2
-        self.soc_start_default = soc_start_default
-        self.soc_end_default = soc_end_default
-        self.soc_start_tier_2 = soc_start_tier_2
-        self.soc_end_tier_2 = soc_end_tier_2
 
-        self.fmg_start_default = fmg_start_default  # defaulted to 1 in case there are None, if not float value
-        self.fmg_end_default = fmg_end_default  # defaulted to 1 in case there are None, if not float value
-        self.fmg_start_tier_2 = fmg_start_tier_2  # tier 2 value, expects float or None
-        self.fmg_end_tier_2 = fmg_end_tier_2  # tier 2 value, expects float or None
-        self.flu_start_default = flu_start_default  # defaulted to 1 in case there are None, if not float value
-        self.flu_end_default = flu_end_default  # defaulted to 1 in case there are None, if not float value
-        self.flu_start_tier_2 = flu_start_tier_2  # tier 2 value, expects float or None
-        self.flu_end_tier_2 = flu_end_tier_2  # tier 2 value, expects float or None
-        self.fi_start_default = fi_start_default  # defaulted to 1 in case there are None, if not float value
-        self.fi_end_default = fi_end_default  # defaulted to 1 in case there are None, if not float value
-        self.fi_start_tier_2 = fi_start_tier_2  # tier 2 value, expects float or None
-        self.fi_end_tier_2 = fi_end_tier_2  # tier 2 value, expects float or None
+@dataclass
+class PerennialCropping(LandModule):
 
-        self.calculate_soc_som = calculate_soc_som
-        self.delay = delay
-
-        # TODO: Assigned FMG, FLU, FI values. Maybe once everything has been done change this structure
-        self.fmg_start = self.fmg_start_tier_2 if self.fmg_start_tier_2 else self.fmg_start_default
-        self.fmg_end = self.fmg_end_tier_2 if self.fmg_end_tier_2 else self.fmg_end_default
-        self.flu_start = self.flu_start_tier_2 if self.flu_start_tier_2 else self.flu_start_default
-        self.flu_end = self.flu_end_tier_2 if self.flu_end_tier_2 else self.flu_end_default
-        self.fi_start = self.fi_start_tier_2 if self.fi_start_tier_2 else self.fi_start_default
-        self.fi_end = self.fi_end_tier_2 if self.fi_end_tier_2 else self.fi_end_default
-
-        self.biomass_start = biomass_start_default if not biomass_start_tier_2 else biomass_start_tier_2
-        self.biomass_end = biomass_end_default if not biomass_end_tier_2 else biomass_end_tier_2
-
-        # Added values
-        self.total_hectars = yearly_time_dependent_parameter_breakdown(area_start, area_end, self.time_impl, self.time_cap, self.rate)
-        self.hectars_before_20, self.hectars_after_20 = yearly_time_dependent_20_year_breakdown(area_start, area_end, self.time_impl, self.time_cap, self.rate)
-
-        self.soc_start = self.soc_start_default * self.fmg_start * self.flu_start * self.fi_start if not self.soc_start_tier_2 else self.soc_start_tier_2
-        self.soc_end = self.soc_end_default * self.fmg_end * self.flu_end * self.fi_end if not self.soc_end_tier_2 else self.soc_end_tier_2
-
-        # TIER 2 DEFAULTS
-        self.soc_start_tier_2_default = self.soc_start_default * self.fmg_start_default * self.flu_start_default * self.fi_start_default
-        self.soc_end_tier_2_default = self.soc_end_default * self.fmg_end_default * self.flu_end_default * self.fi_end_default
-        self.fmg_start_tier_2_default = self.fmg_start_default * self.flu_start_default * self.fi_start_default
-        self.fmg_end_tier_2_default = self.fmg_end_default * self.flu_end_default * self.fi_end_default
-        self.flu_start_tier_2_default = self.flu_start_default * self.fi_start_default
-        self.flu_end_tier_2_default = self.flu_end_default * self.fi_end_default
-        self.fi_start_tier_2_default = self.fi_start_default
-        self.fi_end_tier_2_default = self.fi_end_default
-        self.agb_rate_tier_2_default = self.agb_rate_default
-        self.bgb_rate_tier_2_default = self.bgb_rate_default
-        self.t_biomass_tier_2_default = self.agb_rate_default * 0.5 / 0.47 if not self.agb_rate_tier_2 else self.agb_rate_tier_2 * 0.5 / 0.47
-
-        # RESULTS
-        self.yearly_residue_emissions = []
-        self.total_residue_emissions = 0
-
-        self.yearly_bio_emissions = []
-        self.total_bio_emissions = 0
-
-        self.yearly_som_emissions = []
-        self.total_som_emissions = 0
-
-        self.yearly_soil_emissions = []
-        self.total_soil_emissions = 0
-
-        self.emissions_biomass_yearly = []
-        self.emissions_biomass_total = 0
-
-        self.emissions_total_yearly = []
-        self.total_emissions = 0
-
-        self.result = Result(self.time_impl, self.time_cap)
+    nitrous_constant: float
+    methane_constant: float
+    residue_burnt: float
+    emission_factor_burning_nitrous_residue: float
+    ef_nitrous_som: float
+    emission_factor_burning_methane: float
+    combustion_factor: float
+    fire_periodicity_default: float
+    fire_periodicity_tier_2: Optional[float]
+    t_biomass_tier_2: Optional[float]
+    agb_rate_default: float
+    agb_rate_tier_2: Optional[float]
+    agb_maximum_c: float
+    bgb_rate_default: float
+    bgb_rate_tier_2: Optional[float]
 
     def calculate_emissions(
         self,
@@ -178,12 +61,11 @@ class PerennialCropping(BaseModule):
 
                 co2_crop = (kg_nitrous * self.nitrous_constant + kg_methane * self.methane_constant) / 1000
 
-                total = sum(self.total_hectars) * co2_crop
+                total = sum(self.hectares_total) * co2_crop
 
-                self.yearly_residue_emissions = breakdown_according_to_values(total, self.total_hectars)
-                self.total_residue_emissions = total
+                yearly_residue_emissions = breakdown_according_to_values(total, self.hectares_total)
 
-                residue_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.CO2, [Emission(e, GasTypes.CO2) for e in self.yearly_residue_emissions], ActivityTypes.RESIDUE_BURNING, delay=self.delay)
+                residue_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.CO2, [Emission(e, GasTypes.CO2) for e in yearly_residue_emissions], ActivityTypes.RESIDUE_BURNING, delay=self.delay)
                 self.result.yearly_emissions_by_sector_by_gas.append(residue_emission_set)
             except Exception as e:
                 traceback.print_exc()
@@ -191,9 +73,9 @@ class PerennialCropping(BaseModule):
         def calculate_som():
             try:
                 if self.calculate_soc_som:
-                    self.yearly_som_emissions, self.total_som_emissions = som_emissions(self.soc_end, self.soc_start, self.ef_nitrous_som, self.nitrous_constant, self.hectars_before_20)
+                    yearly_som_emissions, total_som_emissions = som_emissions(self.soc_end, self.soc_start, self.ef_nitrous_som, self.nitrous_constant, self.hectares_before_20)
 
-                    som_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.N2O, [Emission(e, GasTypes.N2O) for e in self.yearly_som_emissions], ActivityTypes.SOM, delay=self.delay)
+                    som_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.N2O, [Emission(e, GasTypes.N2O) for e in yearly_som_emissions], ActivityTypes.SOM, delay=self.delay)
                     self.result.yearly_emissions_by_sector_by_gas.append(som_emission_set)
 
             except Exception as e:
@@ -202,9 +84,9 @@ class PerennialCropping(BaseModule):
         def calculate_soil():
             try:
                 if self.calculate_soc_som:
-                    self.yearly_soil_emissions, self.total_soil_emissions = soil_emissions_2(self.soc_start, self.soc_end, self.total_hectars, self.area_start, self.area_end, self.hectars_before_20)
+                    yearly_soil_emissions, total_soil_emissions = soil_emissions_2(self.soc_start, self.soc_end, self.hectares_total, self.hectares_start, self.hectares_end, self.hectares_before_20)
 
-                    soil_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.N2O, [Emission(e, GasTypes.N2O) for e in self.yearly_soil_emissions], ActivityTypes.SOIL_CO2_CHANGE, delay=self.delay)
+                    soil_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.N2O, [Emission(e, GasTypes.N2O) for e in yearly_soil_emissions], ActivityTypes.SOIL_CO2_CHANGE, delay=self.delay)
                     self.result.yearly_emissions_by_sector_by_gas.append(soil_emission_set)
             except Exception as e:
                 traceback.print_exc()
@@ -215,9 +97,9 @@ class PerennialCropping(BaseModule):
             try:
                 if self.biomass_start and self.biomass_end:
                     # NOTE: This means that we have received values for both (or we have tier 2 values for both)
-                    self.emissions_biomass_yearly, self.emissions_biomass_total = biomass_emissions(self.biomass_end, self.biomass_start, self.area_start, self.area_end, self.rate, self.time_impl, self.time_cap)
+                    emissions_biomass_yearly, emissions_biomass_total = biomass_emissions(self.biomass_end, self.biomass_start, self.hectares_start, self.hectares_end, self.rate_type, self.implementation_time, self.capitalization_time)
 
-                    biomass_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.CO2, [Emission(e, GasTypes.CO2) for e in self.emissions_biomass_yearly], ActivityTypes.BIOMASS, delay=self.delay)
+                    biomass_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.CO2, [Emission(e, GasTypes.CO2) for e in emissions_biomass_yearly], ActivityTypes.BIOMASS, delay=self.delay)
                     self.result.yearly_emissions_by_sector_by_gas.append(biomass_emission_set)
 
                 else:
@@ -234,17 +116,16 @@ class PerennialCropping(BaseModule):
 
                     max_years_growth = max_agb / agb_rate
 
-                    calculated = self.biomass_start + biomass_accumulation_rate * sum(self.total_hectars) 
-                    tabular = (max_agb + bgb_rate * max_years_growth) * self.area_end
+                    calculated = self.biomass_start + biomass_accumulation_rate * sum(self.hectares_total) 
+                    tabular = (max_agb + bgb_rate * max_years_growth) * self.hectares_end
 
-                    total = -min(calculated, tabular) if (max_agb != 0 and self.area_end != 0) else -calculated
+                    total = -min(calculated, tabular) if (max_agb != 0 and self.hectares_end != 0) else -calculated
 
 
                     # NOTE: maybe this should be broken down over max_years_growth or over all years of project depending on whether calculated or tabular is used
-                    self.yearly_bio_emissions = breakdown_according_to_values_for_x_years(total, self.total_hectars, len(self.total_hectars))
-                    self.total_bio_emissions = total
+                    yearly_bio_emissions = breakdown_according_to_values_for_x_years(total, self.hectares_total, len(self.hectares_total))
 
-                    biomass_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.CO2, [Emission(e, GasTypes.CO2) for e in self.yearly_bio_emissions], ActivityTypes.BIOMASS, delay=self.delay)
+                    biomass_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.CO2, [Emission(e, GasTypes.CO2) for e in yearly_bio_emissions], ActivityTypes.BIOMASS, delay=self.delay)
                     self.result.yearly_emissions_by_sector_by_gas.append(biomass_emission_set)
 
             except Exception as e:
@@ -254,10 +135,3 @@ class PerennialCropping(BaseModule):
         calculate_som()
         calculate_soil()
         calculate_biomass_emissions()
-
-        try:
-            self.emissions_total_yearly = [i + j + k + l for i, j, k, l in zip(self.yearly_residue_emissions, self.yearly_bio_emissions, self.yearly_som_emissions, self.yearly_soil_emissions)]
-            self.total_emissions = sum(self.emissions_total_yearly)
-            return self.total_emissions
-        except Exception as e:
-            traceback.print_exc()
