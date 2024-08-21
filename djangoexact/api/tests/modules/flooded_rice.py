a68926ce48cfeebbbd145ee4ce64603ea9111d62
Claudio Lavacca <claudio.lavacca@fao.org>
2024-08-21 12:58:53 +0200

    Add independent module tests
    Refactor test folder structure

--- djangoexact/api/tests/modules/flooded_rice.py
+++ djangoexact/api/tests/modules/flooded_rice.py
@@ -5,18 +5,18 @@ from api.models import *
 from api.serializers import *
 from ipcc.models import *
 
-from .factories import *
+from ..factories import *
 import api.tests.base_test_classes as t
 
 
-class LivestockTest(t.ModuleTest):
+class FloodedRiceTest(t.ModuleTest):
     def __init__(self):
         super().__init__()
-        self.module_type = ModuleType.objects.get(class_name="Livestock")
-        self.create_module(self.module_type)
+        self.module_type = ModuleType.objects.get(class_name="FloodedRice")
+        self.create_module()
 
     def test(self):
         self.calculate_results()
 
 
-LivestockTest().test()
+FloodedRiceTest().test()
