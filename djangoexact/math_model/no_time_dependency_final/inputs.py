eb0e02a879d5aba24fb6f9ce25d1dd5f6f40e5d2
peterwright95 <peter.wright95@gmail.com>
2024-09-09 11:15:56 +0200

    Fix Breakdown NewIrrigation

--- djangoexact/math_model/no_time_dependency_final/inputs.py
+++ djangoexact/math_model/no_time_dependency_final/inputs.py
@@ -253,8 +253,9 @@ class NewIrrigation(BaseModule):
         try:
             ef = self.ef_tier_2 or self.ef_ref
 
-            self.total_emissions = ef * (self.units_end - self.units_start) / 1000
-            self.emissions_total_yearly = yearly_constant_emissions_breakdown(self.total_emissions, self.time_impl, self.time_cap, self.rate_type)
+            self.total_emissions = ef * (self.units_end - self.units_start) / 1000  # to convert the ef from kg to g
+            yearly_units = yearly_time_dependent_parameter_breakdown(self.units_start, self.units_end, self.implementation_time, self.capitalization_time, self.rate_type)
+            emissions_total_yearly = breakdown_according_to_values(self.total_emissions, yearly_units)
 
             new_irrigation_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.CO2, [Emission(e, GasTypes.CO2) for e in emissions_total_yearly], ActivityTypes.NEW_IRRIGATION, delay=self.delay)
             self.result.yearly_emissions_by_sector_by_gas.append(new_irrigation_emission_set)
