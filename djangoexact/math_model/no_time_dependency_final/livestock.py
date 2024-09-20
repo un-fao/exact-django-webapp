00f31bec134886a8028db21fc7d77cd3693e4d9b
peterwright95 <peter.wright95@gmail.com>
2024-09-20 10:28:50 -0400

    Update Livestock Calculations Results

--- djangoexact/math_model/no_time_dependency_final/livestock.py
+++ djangoexact/math_model/no_time_dependency_final/livestock.py
@@ -123,17 +123,31 @@ class Livestock(BaseModule):
                 if len(self.ef_system_methane_start) != len(self.percentage_system_default_start):
                     raise Exception("Length of ef_system_methane_start and percentage_system_default_start should be same")
                 
-                ch4_head_start, self.ch4_system_head_start_tier_2_default, self.percentage_prp_start_tier_2_default = ch4_head_calculation_general(self.tam_start, self.vser_start, self.ef_prp_methane_start, self.percentage_prp_default_start, self.percentage_prp_tier_2_start, self.ef_system_methane_start, self.ch4_prp_tier_2_start, self.percentage_system_default_start, self.ch4_system_default_start, self.ch4_system_tier_2_start, 1000)
-                ch4_head_end, self.ch4_system_head_end_tier_2_default, self.percentage_prp_end_tier_2_default = ch4_head_calculation_general(self.tam_end, self.vser_end, self.ef_prp_methane_end, self.percentage_prp_default_end, self.percentage_prp_tier_2_end, self.ef_system_methane_end, self.ch4_prp_tier_2_end, self.percentage_system_default_end, self.ch4_system_default_end, self.ch4_system_tier_2_end, 1000)
+                ch4_head_start, ch4_system_head_start, percentage_prp_start = ch4_head_calculation_general(self.tam_start, self.vser_start, self.ef_prp_methane_start, self.percentage_prp_default_start, self.percentage_prp_tier_2_start, self.ef_system_methane_start, self.ch4_prp_tier_2_start, self.percentage_system_default_start, self.ch4_system_default_start, self.ch4_system_tier_2_start, 1000)
+                ch4_head_end, ch4_system_head_end, percentage_prp_end = ch4_head_calculation_general(self.tam_end, self.vser_end, self.ef_prp_methane_end, self.percentage_prp_default_end, self.percentage_prp_tier_2_end, self.ef_system_methane_end, self.ch4_prp_tier_2_end, self.percentage_system_default_end, self.ch4_system_default_end, self.ch4_system_tier_2_end, 1000)
+                
+                # NOTE: in future change name of percentage_prp_tier_2_default as it represents ch4_prp (TIER 2 Assignment)
+                self.ch4_system_head_start_tier_2_default = ch4_system_head_start
+                self.percentage_prp_start_tier_2_default = percentage_prp_start
+                self.ch4_system_head_end_tier_2_default = ch4_system_head_end
+                self.percentage_prp_end_tier_2_default = percentage_prp_end
 
-                annual_start = ch4_head_start * self.head_number_start / 1000 * self.methane_constant
-                annual_end = ch4_head_end * self.head_number_end / 1000 * self.methane_constant
+                # Calculate emissions, broken down between prp and system
+                annual_start_system = sum(ch4_system_head_start) * self.head_number_start / 1000 * self.methane_constant
+                annual_end_system = sum(ch4_system_head_end) * self.head_number_end / 1000 * self.methane_constant
 
-                mmm_emissions_yearly = yearly_time_dependent_parameter_breakdown(annual_start, annual_end, self.implementation_time, self.capitalization_time, self.rate_type)
-                mmm_emissions = sum(mmm_emissions_yearly)
+                mmm_emissions_system_yearly = yearly_time_dependent_parameter_breakdown(annual_start_system, annual_end_system, self.implementation_time, self.capitalization_time, self.rate_type)
 
-                mmm_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.CH4, [Emission(e, GasTypes.CH4) for e in mmm_emissions_yearly], ActivityTypes.METHANE_MANURE_MANAGEMENT, delay=self.delay)
+                annual_start_prp = percentage_prp_start * self.head_number_start / 1000 * self.methane_constant
+                annual_end_prp = percentage_prp_end * self.head_number_end / 1000 * self.methane_constant
+
+                mmm_emissions_prp_yearly = yearly_time_dependent_parameter_breakdown(annual_start_prp, annual_end_prp, self.implementation_time, self.capitalization_time, self.rate_type)
+
+                mmm_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.CH4, [Emission(e, GasTypes.CH4) for e in mmm_emissions_system_yearly], ActivityTypes.METHANE_MANURE_MANAGEMENT_SYSTEM, delay=self.delay)
                 self.result.yearly_emissions_by_sector_by_gas.append(mmm_emission_set)
+
+                mmm_emission_set_prp = YearlyGasActivityEmissionSet(0, GasTypes.CH4, [Emission(e, GasTypes.CH4) for e in mmm_emissions_prp_yearly], ActivityTypes.METHANE_MANURE_MANAGEMENT_PRP, delay=self.delay)
+                self.result.yearly_emissions_by_sector_by_gas.append(mmm_emission_set_prp)
 
             except Exception as e:
                 traceback.print_exc()
@@ -143,17 +157,30 @@ class Livestock(BaseModule):
                 if len(self.ef_system_nitrous_direct_start) != len(self.percentage_system_default_start):
                     raise Exception("Length of ef_system_nitrous_direct_start and percentage_system_default_start should be same")
                 
-                n2o_head_start, self.n2o_system_direct_head_start_tier_2_default, self.n2o_prp_direct_head_start_tier_2_default  = ch4_head_calculation_general(self.tam_start, self.ner_start, self.ef_prp_nitrous_direct_start, self.percentage_prp_default_start, self.percentage_prp_tier_2_start, self.ef_system_nitrous_direct_start, self.n2o_prp_tier_2_start_direct, self.percentage_system_default_start, self.n2o_system_direct_default_start, self.n2o_system_direct_tier_2_start)
-                n2o_head_end, self.n2o_system_direct_head_end_tier_2_default, self.n2o_prp_direct_head_end_tier_2_default = ch4_head_calculation_general(self.tam_end, self.ner_end, self.ef_prp_nitrous_direct_end, self.percentage_prp_default_end, self.percentage_prp_tier_2_end, self.ef_system_nitrous_direct_end, self.n2o_prp_tier_2_end_direct, self.percentage_system_default_end, self.n2o_system_direct_default_end, self.n2o_system_direct_tier_2_end)
+                n2o_head_start, n2o_system_direct_head_start, n2o_prp_direct_head_start  = ch4_head_calculation_general(self.tam_start, self.ner_start, self.ef_prp_nitrous_direct_start, self.percentage_prp_default_start, self.percentage_prp_tier_2_start, self.ef_system_nitrous_direct_start, self.n2o_prp_tier_2_start_direct, self.percentage_system_default_start, self.n2o_system_direct_default_start, self.n2o_system_direct_tier_2_start)
+                n2o_head_end, n2o_system_direct_head_end, n2o_prp_direct_head_end = ch4_head_calculation_general(self.tam_end, self.ner_end, self.ef_prp_nitrous_direct_end, self.percentage_prp_default_end, self.percentage_prp_tier_2_end, self.ef_system_nitrous_direct_end, self.n2o_prp_tier_2_end_direct, self.percentage_system_default_end, self.n2o_system_direct_default_end, self.n2o_system_direct_tier_2_end)
 
-                annual_start = n2o_head_start * self.head_number_start / 1000 * 44 / 28 * self.nitrous_constant
-                annual_end = n2o_head_end * self.head_number_end / 1000 * 44 / 28 * self.nitrous_constant
+                self.n2o_system_direct_head_start_tier_2_default = n2o_system_direct_head_start
+                self.n2o_prp_direct_head_start_tier_2_default = n2o_prp_direct_head_start
+                self.n2o_system_direct_head_end_tier_2_default = n2o_system_direct_head_end
+                self.n2o_prp_direct_head_end_tier_2_default = n2o_prp_direct_head_end
 
-                nmm_direct_emissions_yearly = yearly_time_dependent_parameter_breakdown(annual_start, annual_end, self.implementation_time, self.capitalization_time, self.rate_type)
-                nmm_direct_emissions = sum(nmm_direct_emissions_yearly)
+                # SYSTEM AND PRP EMISSIONS
 
-                nmm_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.N2O, [Emission(e, GasTypes.N2O) for e in nmm_direct_emissions_yearly], ActivityTypes.NITROUS_MANURE_MANAGEMENT, delay=self.delay)
-                self.result.yearly_emissions_by_sector_by_gas.append(nmm_emission_set)
+                annual_start_system_n2o = sum(n2o_system_direct_head_start) * self.head_number_start / 1000 * 44 / 28 * self.nitrous_constant
+                annual_end_system_n2o = sum(n2o_system_direct_head_end) * self.head_number_end / 1000 * 44 / 28 * self.nitrous_constant
+
+                nmm_direct_emissions_yearly = yearly_time_dependent_parameter_breakdown(annual_start_system_n2o, annual_end_system_n2o, self.implementation_time, self.capitalization_time, self.rate_type)
+
+                nmm_emission_set_system = YearlyGasActivityEmissionSet(0, GasTypes.N2O, [Emission(e, GasTypes.N2O) for e in nmm_direct_emissions_yearly], ActivityTypes.NITROUS_MANURE_MANAGEMENT_SYSTEM, delay=self.delay)
+                self.result.yearly_emissions_by_sector_by_gas.append(nmm_emission_set_system)
+
+                annual_start_prp_n2o = n2o_prp_direct_head_start * self.head_number_start / 1000 * 44 / 28 * self.nitrous_constant
+                annual_end_prp_n2o = n2o_prp_direct_head_end * self.head_number_end / 1000 * 44 / 28 * self.nitrous_constant
+
+                nmm_emissions_prp_yearly = yearly_time_dependent_parameter_breakdown(annual_start_prp_n2o, annual_end_prp_n2o, self.implementation_time, self.capitalization_time, self.rate_type)
+
+                nmm_emission_set_prp = YearlyGasActivityEmissionSet(0, GasTypes.N2O, [Emission(e, GasTypes.N2O) for e in nmm_emissions_prp_yearly], ActivityTypes.NITROUS_MANURE_MANAGEMENT_PRP, delay=self.delay)
 
             except Exception as e:
                 traceback.print_exc()
@@ -164,18 +191,29 @@ class Livestock(BaseModule):
                 if len(self.ef_system_nitrous_indirect_volatization_start) != len(self.percentage_system_default_start):
                     raise Exception("Length of ef_system_nitrous_indirect_volatization_start and percentage_system_default_start should be same")
                 
-                n2o_head_start, _, _ = ch4_head_calculation_general(self.tam_start, self.ner_start, self.ef_prp_nitrous_indirect_volatization_start, self.percentage_prp_default_start, self.percentage_prp_tier_2_start, self.ef_system_nitrous_indirect_volatization_start, self.n2o_prp_tier_2_start_indirect_volatization, self.percentage_system_default_start, self.n20_system_indirect_volatization_default_start, self.n20_system_indirect_volatization_tier_2_start)
-                n2o_head_end, _, _ = ch4_head_calculation_general(self.tam_end, self.ner_end, self.ef_prp_nitrous_indirect_volatization_end, self.percentage_prp_default_end, self.percentage_prp_tier_2_end, self.ef_system_nitrous_indirect_volatization_end, self.n2o_prp_tier_2_end_indirect_volatization, self.percentage_system_default_end, self.n20_system_indirect_volatization_default_end, self.n20_system_indirect_volatization_tier_2_end)
+                n2o_head_start, n2o_head_system_start, n2o_head_prp_start = ch4_head_calculation_general(self.tam_start, self.ner_start, self.ef_prp_nitrous_indirect_volatization_start, self.percentage_prp_default_start, self.percentage_prp_tier_2_start, self.ef_system_nitrous_indirect_volatization_start, self.n2o_prp_tier_2_start_indirect_volatization, self.percentage_system_default_start, self.n20_system_indirect_volatization_default_start, self.n20_system_indirect_volatization_tier_2_start)
+                n2o_head_end, n2o_heah_system_end, n2o_head_prp_end = ch4_head_calculation_general(self.tam_end, self.ner_end, self.ef_prp_nitrous_indirect_volatization_end, self.percentage_prp_default_end, self.percentage_prp_tier_2_end, self.ef_system_nitrous_indirect_volatization_end, self.n2o_prp_tier_2_end_indirect_volatization, self.percentage_system_default_end, self.n20_system_indirect_volatization_default_end, self.n20_system_indirect_volatization_tier_2_end)
 
-                annual_start = n2o_head_start * self.head_number_start / 1000 * 44 / 28 * self.nitrous_constant * self.volatilization_multiplier
-                annual_end = n2o_head_end * self.head_number_end / 1000 * 44 / 28 * self.nitrous_constant * self.volatilization_multiplier
+                annual_start_system = sum(n2o_head_system_start) * self.head_number_start / 1000 * 44 / 28 * self.nitrous_constant * self.volatilization_multiplier
+                annual_end_system = sum(n2o_heah_system_end) * self.head_number_end / 1000 * 44 / 28 * self.nitrous_constant * self.volatilization_multiplier
 
-                nmm_indirect_volatization_emissions_yearly = yearly_time_dependent_parameter_breakdown(annual_start, annual_end, self.implementation_time, self.capitalization_time, self.rate_type)
-                nmm_indirect_volatization_emissions = sum(nmm_indirect_volatization_emissions_yearly)
 
-                nmm_indirect_volatization_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.N2O, [Emission(e, GasTypes.N2O) for e in nmm_indirect_volatization_emissions_yearly], ActivityTypes.NITROUS_MANURE_MANAGEMENT_INDIRECT_VOLATILIZATION, delay=self.delay)
+                # NOTE MAYBE A GENERALIZED FUNCTION CAN BE DONE FOR THIS NOW?
+                nmm_indirect_volatization_emissions_yearly_system = yearly_time_dependent_parameter_breakdown(annual_start_system, annual_end_system, self.implementation_time, self.capitalization_time, self.rate_type)
 
-                self.result.yearly_emissions_by_sector_by_gas.append(nmm_indirect_volatization_emission_set)
+                nmm_indirect_volatization_emission_set_system = YearlyGasActivityEmissionSet(0, GasTypes.N2O, [Emission(e, GasTypes.N2O) for e in nmm_indirect_volatization_emissions_yearly_system], ActivityTypes.NITROUS_MANURE_MANAGEMENT_INDIRECT_VOLATILIZATION_SYSTEM, delay=self.delay)
+
+                self.result.yearly_emissions_by_sector_by_gas.append(nmm_indirect_volatization_emission_set_system)
+
+                annual_start_prp = n2o_head_prp_start * self.head_number_start / 1000 * 44 / 28 * self.nitrous_constant * self.volatilization_multiplier
+                annual_end_prp = n2o_head_prp_end * self.head_number_end / 1000 * 44 / 28 * self.nitrous_constant * self.volatilization_multiplier
+
+                nmm_indirect_volatization_emissions_yearly_prp = yearly_time_dependent_parameter_breakdown(annual_start_prp, annual_end_prp, self.implementation_time, self.capitalization_time, self.rate_type)
+
+                nmm_indirect_volatization_emission_set_prp = YearlyGasActivityEmissionSet(0, GasTypes.N2O, [Emission(e, GasTypes.N2O) for e in nmm_indirect_volatization_emissions_yearly_prp], ActivityTypes.NITROUS_MANURE_MANAGEMENT_INDIRECT_VOLATILIZATION_PRP, delay=self.delay)
+
+                self.result.yearly_emissions_by_sector_by_gas.append(nmm_indirect_volatization_emission_set_prp)
+
 
             except Exception as e:
                 traceback.print_exc()
@@ -186,18 +224,26 @@ class Livestock(BaseModule):
                 if len(self.ef_system_nitrous_indirect_leaching_start) != len(self.percentage_system_default_start):
                     raise Exception("Length of ef_system_nitrous_indirect_leaching_start and percentage_system_default_start should be same")
                 
-                n2o_head_start, _, _ = ch4_head_calculation_general(self.tam_start, self.ner_start, self.ef_prp_nitrous_indirect_leaching_start, self.percentage_prp_default_start, self.percentage_prp_tier_2_start, self.ef_system_nitrous_indirect_leaching_start, self.n2o_prp_tier_2_start_indirect_leaching, self.percentage_system_default_start, self.n20_system_indirect_leaching_default_start, self.n20_system_indirect_leaching_tier_2_start)
-                n2o_head_end, _, _ = ch4_head_calculation_general(self.tam_end, self.ner_end, self.ef_prp_nitrous_indirect_leaching_end, self.percentage_prp_default_end, self.percentage_prp_tier_2_end, self.ef_system_nitrous_indirect_leaching_end, self.n2o_prp_tier_2_end_indirect_leaching, self.percentage_system_default_end, self.n20_system_indirect_leaching_default_end, self.n20_system_indirect_leaching_tier_2_end)
+                n2o_head_start, n2o_system_start, n2o_prp_start = ch4_head_calculation_general(self.tam_start, self.ner_start, self.ef_prp_nitrous_indirect_leaching_start, self.percentage_prp_default_start, self.percentage_prp_tier_2_start, self.ef_system_nitrous_indirect_leaching_start, self.n2o_prp_tier_2_start_indirect_leaching, self.percentage_system_default_start, self.n20_system_indirect_leaching_default_start, self.n20_system_indirect_leaching_tier_2_start)
+                n2o_head_end, n2o_system_end, n2o_prp_end = ch4_head_calculation_general(self.tam_end, self.ner_end, self.ef_prp_nitrous_indirect_leaching_end, self.percentage_prp_default_end, self.percentage_prp_tier_2_end, self.ef_system_nitrous_indirect_leaching_end, self.n2o_prp_tier_2_end_indirect_leaching, self.percentage_system_default_end, self.n20_system_indirect_leaching_default_end, self.n20_system_indirect_leaching_tier_2_end)
 
-                annual_start = n2o_head_start * self.head_number_start / 1000 * 44 / 28 * self.nitrous_constant * self.leaching_multiplier
-                annual_end = n2o_head_end * self.head_number_end / 1000 * 44 / 28 * self.nitrous_constant * self.leaching_multiplier
+                annual_start_system = sum(n2o_system_start) * self.head_number_start / 1000 * 44 / 28 * self.nitrous_constant * self.leaching_multiplier
+                annual_end_system = sum(n2o_system_end) * self.head_number_end / 1000 * 44 / 28 * self.nitrous_constant * self.leaching_multiplier
 
-                nmm_indirect_leaching_emissions_yearly = yearly_time_dependent_parameter_breakdown(annual_start, annual_end, self.implementation_time, self.capitalization_time, self.rate_type)
-                nmm_indirect_leaching_emissions = sum(nmm_indirect_leaching_emissions_yearly)
+                nmm_indirect_leaching_emissions_yearly_system = yearly_time_dependent_parameter_breakdown(annual_start_system, annual_end_system, self.implementation_time, self.capitalization_time, self.rate_type)
 
-                nmm_indirect_leaching_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.N2O, [Emission(e, GasTypes.N2O) for e in nmm_indirect_leaching_emissions_yearly], ActivityTypes.NITROUS_MANURE_MANAGEMENT_INDIRECT_LEACHING, delay=self.delay)
+                nmm_indirect_leaching_emission_set_system = YearlyGasActivityEmissionSet(0, GasTypes.N2O, [Emission(e, GasTypes.N2O) for e in nmm_indirect_leaching_emissions_yearly_system], ActivityTypes.NITROUS_MANURE_MANAGEMENT_INDIRECT_LEACHING_SYSTEM, delay=self.delay)
 
-                self.result.yearly_emissions_by_sector_by_gas.append(nmm_indirect_leaching_emission_set)
+                self.result.yearly_emissions_by_sector_by_gas.append(nmm_indirect_leaching_emission_set_system)
+
+                annual_start_prp = n2o_prp_start * self.head_number_start / 1000 * 44 / 28 * self.nitrous_constant * self.leaching_multiplier
+                annual_end_prp = n2o_prp_end * self.head_number_end / 1000 * 44 / 28 * self.nitrous_constant * self.leaching_multiplier
+
+                nmm_indirect_leaching_emissions_yearly_prp = yearly_time_dependent_parameter_breakdown(annual_start_prp, annual_end_prp, self.implementation_time, self.capitalization_time, self.rate_type)
+
+                nmm_indirect_leaching_emission_set_prp = YearlyGasActivityEmissionSet(0, GasTypes.N2O, [Emission(e, GasTypes.N2O) for e in nmm_indirect_leaching_emissions_yearly_prp], ActivityTypes.NITROUS_MANURE_MANAGEMENT_INDIRECT_LEACHING_PRP, delay=self.delay)
+
+                self.result.yearly_emissions_by_sector_by_gas.append(nmm_indirect_leaching_emission_set_prp)
 
             except Exception as e:
                 traceback.print_exc()
