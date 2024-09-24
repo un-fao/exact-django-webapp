3ee2bfc924d70f6c0fc04e9ec5a8868b0a87adaa
Claudio Lavacca <claudio.lavacca@fao.org>
2024-09-24 17:47:48 +0200

    Add types po files and creation script

--- djangoexact/api/admin.py
+++ djangoexact/api/admin.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
-from modeltranslation.admin import TranslationAdmin
-
 from .models import *
-
-
-class GenericAdmin(ModelAdmin, TranslationAdmin):
-    pass
 
 
 for model in [model for model in dir() if not model.startswith("_")]:
