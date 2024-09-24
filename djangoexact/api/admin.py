c3de2aa0cac73b10c122c9cf06f9e88b932b4f4f
Claudio Lavacca <claudio.lavacca@fao.org>
2024-09-24 16:00:56 +0200

    Add model translations

--- djangoexact/api/admin.py
+++ djangoexact/api/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
+from modeltranslation.admin import TranslationAdmin
 
 from .models import *
+
+
+class GenericAdmin(ModelAdmin, TranslationAdmin):
+    pass
+
 
 for model in [model for model in dir() if not model.startswith("_")]:
     try:
