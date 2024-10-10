b61a8d8ea50ebf5136b6955ccb6b5f22561fff02
Claudio Lavacca <claudio.lavacca@fao.org>
2024-10-10 16:08:16 +0200

    Add forest management test case

---
+++ djangoexact/api/tests/modules/forest_management.py
@@ -0,0 +1,22 @@
+import logging as log
+
+from api.calculators import *
+from api.models import *
+from api.serializers import *
+from ipcc.models import *
+
+from ..factories import *
+import api.tests.base_test_classes as t
+
+
+class GrasslandTest(t.ModuleTest):
+    def __init__(self):
+        super().__init__()
+        self.module_type = ModuleType.objects.get(class_name="ForestManagement")
+        self.create_module()
+
+    def test(self):
+        self.calculate_results()
+
+
+GrasslandTest().test()
