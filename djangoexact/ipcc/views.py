14362e3c995c3ffd01274554c6ebd7f6010bb154
Claudio Lavacca <claudio.lavacca@fao.org>
2024-10-17 21:58:24 +0200

    Add basic data viewer for ipcc tables

--- djangoexact/ipcc/views.py
+++ djangoexact/ipcc/views.py
@@ -3,6 +3,11 @@ from django.core.exceptions import FieldDoesNotExist
 from django.db.models import Model
 from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated
+from django.apps import apps
+from django.shortcuts import render
+from django.db.models import Q
+from django.db import models
+
 
 from .serializers import get_model_serializer
 
@@ -36,3 +41,56 @@ def generic_viewset(_model: Model):
             return super().get_queryset()
 
     return GenericViewSet
+
+
+def table_list_view(request):
+    models = apps.get_app_config("ipcc").get_models()
+    models_info = []
+    for model in models:
+        models_info.append({"verbose_name": model._meta.verbose_name_plural, "model_name": model.__name__})
+
+    context = {"models_info": models_info}
+    return render(request, "table_list.html", context)
+
+
+def table_data_view(request, model_name):
+    # Get the model class
+    model = apps.get_model("ipcc", model_name)
+
+    # Get field names and verbose names
+    fields = [field.name for field in model._meta.fields]
+    verbose_fields = [field.verbose_name for field in model._meta.fields]
+
+    # Fetch data for the model
+    query_string = request.GET.get("q", "").strip()
+    data = model.objects.all()
+
+    if query_string:
+        q_object = Q()
+        queries = query_string.split()  # Split the query by spaces
+
+        for query in queries:
+            sub_q = Q()
+            for field in model._meta.fields:
+                if isinstance(field, (models.CharField, models.TextField)):
+                    sub_q |= Q(**{f"{field.name}__icontains": query})
+                elif isinstance(field, (models.FloatField, models.IntegerField)):
+                    try:
+                        query_value = float(query)
+                        sub_q |= Q(**{f"{field.name}": query_value})
+                    except ValueError:
+                        pass
+                elif isinstance(field, models.ForeignKey):
+                    sub_q |= Q(**{f"{field.name}__name__icontains": query})
+            q_object &= sub_q
+
+        data = model.objects.filter(q_object)
+
+    context = {
+        "model_name": model._meta.verbose_name_plural,
+        "fields": fields,
+        "verbose_fields": verbose_fields,
+        "data": data,
+        "query": query_string,
+    }
+    return render(request, "table_data.html", context)
