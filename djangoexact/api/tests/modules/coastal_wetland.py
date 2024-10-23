daaa033ee89c8a9342c53062e1c0a1b83cad7c3b
Claudio Lavacca <claudio.lavacca@fao.org>
2024-10-23 12:44:49 +0200

    Add logging of modules parameters in base module test class

--- djangoexact/api/tests/modules/coastal_wetland.py
+++ djangoexact/api/tests/modules/coastal_wetland.py
@@ -12,6 +12,7 @@ import api.tests.base_test_classes as t
 class CoastalWetlandTest(t.ModuleTest):
     def __init__(self):
         super().__init__()
+        self.parameters_to_print = ["land_use_type"]
         self.module_type = ModuleType.objects.get(class_name="CoastalWetland")
         self.create_module()
 
