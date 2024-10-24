82970ec6636197b0ffd44757e2424f349cc17e09
Claudio Lavacca <claudio.lavacca@fao.org>
2024-10-24 13:02:34 +0200

    Fix unfold admin imports
    Cut automatically fileed fields from blog model admin

--- djangoexact/djangoexact/settings.py
+++ djangoexact/djangoexact/settings.py
@@ -45,6 +45,11 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 INSTALLED_APPS = [
     "unfold",  # before django.contrib.admin
+    "unfold.contrib.filters",  # optional, if special filters are needed
+    "unfold.contrib.forms",  # optional, if special form elements are needed
+    "unfold.contrib.inlines",  # optional, if special inlines are needed
+    "unfold.contrib.import_export",  # optional, if django-import-export package is used
+    "unfold.contrib.guardian",  # optional, if django-guardian package is used
     "unfold.contrib.simple_history",  # optional, if django-simple-history package is used
     "modeltranslation",
     "django.contrib.admin",
