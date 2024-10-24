097ce8179d2d9d04e5e35b9a29692304bf400a5c
Claudio Lavacca <claudio.lavacca@fao.org>
2024-10-24 13:02:34 +0200

    Fix unfold admin imports
    Cut automatically fileed fields from blog model admin

--- djangoexact/blog/admin.py
+++ djangoexact/blog/admin.py
@@ -3,20 +3,30 @@ from django.contrib import admin
 # Register your models here.
 from django import forms
 from django.contrib import admin
-from ckeditor.widgets import CKEditorWidget
 from blog.models import Post
+import slugify.slugify as slugify
+from unfold.contrib.forms.widgets import WysiwygWidget
+from django.db import models
+from unfold.admin import ModelAdmin
 
 
 class PostAdminForm(forms.ModelForm):
-    content = forms.CharField(widget=CKEditorWidget())
+    content = forms.CharField(widget=WysiwygWidget)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+    author = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
         model = Post
         fields = "__all__"
 
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(ModelAdmin):
     form = PostAdminForm
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        obj.slug = slugify(obj.title)
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(Post, PostAdmin)
