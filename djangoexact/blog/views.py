b13cee6600759bee499d36bcec4e592491a5c339
Claudio Lavacca <claudio.lavacca@fao.org>
2024-10-24 13:27:29 +0200

    Add blog posts viewset as api endpoint

--- djangoexact/blog/views.py
+++ djangoexact/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 import blog.models as models
-
-# Create your views here.
+from rest_framework import viewsets
+import blog.serializers as serializers
+from api.views import AuthenticatedViewSet
 
 
 def home(request):
@@ -12,3 +13,11 @@ def home(request):
 def post_detail(request, slug):
     post = models.Post.objects.get(slug=slug)
     return render(request, "post_detail.html", {"post": post})
+
+
+class PostViewSet(viewsets.ModelViewSet, AuthenticatedViewSet):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
