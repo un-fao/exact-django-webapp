5765d8b59eed9a5a5c9e623fec50df24d16fbbdd
Claudio Lavacca <claudio.lavacca@fao.org>
2024-10-28 09:32:42 +0100

    Fix agb fetch in deforestation calculator

--- djangoexact/api/calculators.py
+++ djangoexact/api/calculators.py
@@ -750,7 +750,7 @@ class DeforestationCalculator(BaseCalculator):
         # TODO: Review with new forest management data
         if forest.land_use_type_start.name != utils.MANGROVES:
             try:
-                agb_start = ipcc.ForestManagementAGB.objects.get(climate=climate, region=region, forest_type=forest.forest_type, land_use_type=forest.land_use_type_start, forest_condition_type=forest.forest_condition_type)
+                agb_start = forest.get_agb_growth_ref(land_use_type=forest.land_use_type_start, from_year=21)
             except ipcc.ForestManagementAGB.DoesNotExist:
                 if forest.agb_t2_start is None:
                     raise Exception(f"AGB for {forest.land_use_type_start} in {climate} climate, {region} region, and {forest.forest_type} forest type does not exist")
@@ -777,7 +777,7 @@ class DeforestationCalculator(BaseCalculator):
                 except ipcc.LitterDeadwoodCarbonStock.DoesNotExist:
                     raise Exception(f"LitterDeadwoodCarbonStock for {module.land_use_type_w} in {climate} climate, {forest.forest_type} forest type does not exist")
                 try:
-                    agb_w = ipcc.ForestManagementAGB.objects.get(climate=climate, region=region, forest_type=forest.forest_type, land_use_type=forest.land_use_type_w, forest_condition_type=forest.forest_condition_type)
+                    agb_w = forest.get_agb_growth_ref(land_use_type=forest.land_use_type_w, from_year=21)
                 except ipcc.ForestManagementAGB.DoesNotExist:
                     if forest.agb_t2_w is None:
                         raise Exception(f"AGB for {forest.land_use_type_w} in {climate} climate, {region} region, and {forest.forest_type} forest type does not exist")
@@ -804,7 +804,7 @@ class DeforestationCalculator(BaseCalculator):
                 except ipcc.LitterDeadwoodCarbonStock.DoesNotExist:
                     raise Exception(f"LitterDeadwoodCarbonStock for {module.land_use_type_wo.name} in {climate.name} climate, {forest.forest_type.name} forest type does not exist")
                 try:
-                    agb_wo = ipcc.ForestManagementAGB.objects.get(climate=climate, region=region, forest_type=forest.forest_type, land_use_type=forest.land_use_type_wo, forest_condition_type=forest.forest_condition_type)
+                    agb_wo = forest.get_agb_growth_ref(land_use_type=forest.land_use_type_wo, from_year=21)
                 except ipcc.ForestManagementAGB.DoesNotExist:
                     if forest.agb_t2_wo is None:
                         raise Exception(f"AGB for {forest.land_use_type_wo} in {climate} climate, {region} region, and {forest.forest_type} forest type does not exist")
