4dca7e2b34b9df09b3f6c520d22655e095ead221
Claudio Lavacca <claudio.lavacca@fao.org>
2024-11-04 14:12:42 +0100

    Cut direct road type name reference from road calculator error message
    Refactor road type model tostring so it only displays the name

--- djangoexact/api/models.py
+++ djangoexact/api/models.py
@@ -2350,7 +2350,7 @@ class RoadType(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
-        return f"({self.id}) {self.name}"
+        return self.name
 
 
 class Building(Submodule):
