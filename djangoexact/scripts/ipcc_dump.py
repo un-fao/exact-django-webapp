3ac1dcf9aef995fc44efe0e4338186f4a5ba5d38
Claudio Lavacca <claudio.lavacca@fao.org>
2024-11-06 17:15:46 +0100

    Add missing packaging type and emission factor ipcc data

--- djangoexact/scripts/ipcc_dump.py
+++ djangoexact/scripts/ipcc_dump.py
@@ -4462,6 +4462,9 @@ for i, row in df.iterrows():
 
     PackagingType.objects.create(name=name)
 
+log.debug("Deleting all ValueChainPackagingEmissionFactor objects...")
+ValueChainPackagingEmissionFactor.objects.all().delete()
+
 df = pd.read_csv(
     os.path.join(os.path.dirname(__file__), "ipcc_data", "ValueChainPackagingEmissionFactor.csv"),
     header=0,
