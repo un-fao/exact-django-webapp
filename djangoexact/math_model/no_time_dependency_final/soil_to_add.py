c478ac73b5612f6706113b486d090b91ab9f6f6a
peterwright95 <peter.wright95@gmail.com>
2024-11-14 12:01:03 +0100

    Change function names and relative imports

--- djangoexact/math_model/no_time_dependency_final/soil_to_add.py
+++ djangoexact/math_model/no_time_dependency_final/soil_to_add.py
@@ -2,11 +2,11 @@ import math
 import traceback
 
 from .general_functions import (
-    breakdown_according_to_values,
-    soil_emissions_2,
-    yearly_constant_emissions_breakdown,
-    yearly_time_dependent_20_year_breakdown,
-    yearly_time_dependent_parameter_breakdown,
+    breakdown_proportionally_to_values,
+    soil_emissions,
+    breakdown_equally_across_years,
+    compute_half_year_cumulative_n_year_maturity,
+    compute_yearly_or_half_year_cumulative,
 )
 from .ghg_emissions_classes import (
     ActivityTypes,
@@ -26,7 +26,7 @@ class SoilToAdd(LandModule):
     def calculate_emissions(self):
         try:
             if self.calculate_soc_som:
-                self.soil_emissions_yearly, self.soil_emissions_total = soil_emissions_2(self.soc_start, self.soc_end, self.hectares_total, self.hectares_start, self.hectares_end, self.hectares_before_20)
+                self.soil_emissions_yearly, self.soil_emissions_total = soil_emissions(self.soc_start, self.soc_end, self.hectares_total, self.hectares_start, self.hectares_end, self.hectares_before_20)
 
                 soil_emission_set = YearlyGasActivityEmissionSet(0, GasTypes.CO2, [Emission(e, GasTypes.CO2) for e in self.soil_emissions_yearly], ActivityTypes.SOIL_CO2_CHANGE, delay=self.delay)
                 self.result.yearly_emissions_by_sector_by_gas.append(soil_emission_set)
