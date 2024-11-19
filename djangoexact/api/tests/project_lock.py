824a8fa2d197528587f1404bdc1185540bea5d02
Claudio Lavacca <claudio.lavacca@fao.org>
2024-11-19 13:40:30 +0100

    Fix missing logic allowing any user to modify locked projects in modules and activities viewsets

--- djangoexact/api/tests/project_lock.py
+++ djangoexact/api/tests/project_lock.py
@@ -1,12 +1,13 @@
 from rest_framework.test import APIRequestFactory, APITestCase
 from rest_framework import status
 from django.urls import reverse
-from api.views import ProjectViewSet
+from api.views import ProjectViewSet, ActivityViewSet, generic_module_viewset
 import api.models as models
 import ipcc.models as ipcc_models
 import api.tests.factories as factories
 from rest_framework.test import force_authenticate
 from factory.fuzzy import FuzzyText, FuzzyInteger, FuzzyChoice
+import logging as log
 
 
 class ProjectLockTestCase(APITestCase):
@@ -34,6 +35,8 @@ class ProjectLockTestCase(APITestCase):
             - "soil_type": The ID of the selected SoilType instance.
             - "gw_potential": The ID of a randomly selected GlobalWarmingPotential instance.
         """
+        log.getLogger().setLevel(log.INFO)
+        log.info("Setting up ProjectLockTestCase")
         self.user = models.CustomUser.objects.get(email="claudio.lavacca@fao.org")
         self.user2 = models.CustomUser.objects.get(email="test@user.org")
         self.group = models.Group.objects.get(name="Second Reviewer")
@@ -53,6 +56,126 @@ class ProjectLockTestCase(APITestCase):
             "gw_potential": ipcc_models.GlobalWarmingPotential.objects.order_by("?").first().id,
         }
 
+    def create_project(self):
+        """
+        Create a project using the ProjectViewSet.
+
+        This method creates a project by sending a POST request to the 'project-list' endpoint
+        with the provided project data in JSON format. The request is authenticated with the test user,
+        and the response is returned.
+        """
+        log.info("Creating project")
+        factory = APIRequestFactory(enforce_csrf_checks=False)
+        view = ProjectViewSet.as_view({"post": "create"})
+
+        request = factory.post(
+            reverse("project-list"),
+            self.project_data,
+            format="json",
+        )
+        force_authenticate(request, user=self.user)
+        return view(request)
+
+    def create_project_membership(self, project, user):
+        """
+        Create a project membership for a user.
+
+        This method creates a project membership for the provided user and project.
+        """
+        log.info("Creating project membership")
+        return models.ProjectMembership.objects.create(user=user, project=project, group=self.group)
+
+    def edit_project(self, project, user, data):
+        """
+        Edit a project using the ProjectViewSet.
+
+        This method edits a project by sending a PATCH request to the 'project-detail' endpoint
+        with the provided project data in JSON format. The request is authenticated with the provided user,
+        and the response is returned.
+        """
+        log.info("Editing project")
+        factory = APIRequestFactory(enforce_csrf_checks=False)
+        view = ProjectViewSet.as_view({"patch": "partial_update"})
+
+        request = factory.patch(
+            reverse("project-detail", args=[project.id]),
+            data,
+            format="json",
+        )
+        force_authenticate(request, user=user)
+        return view(request, pk=project.id)
+
+    def create_activity(self, project, user):
+        """
+        Create an activity using the ActivityViewSet.
+
+        This method creates an activity by sending a POST request to the 'activity-build' endpoint
+        with the provided activity data in JSON format. The request is authenticated with the provided user,
+        and the response is returned.
+        """
+        log.info("Creating activity")
+        module_type = models.ModuleType.objects.get(class_name="PerennialCropland")
+        change_rate = models.ChangeRate.objects.get(name="linear")
+
+        activity_builder_data = {
+            "name": FuzzyText().fuzz(),
+            "project": project.id,
+            "module_types": [module_type.id],
+            "area": 100,
+            "change_rate": change_rate.id,
+            "cost": 0,
+        }
+
+        factory = APIRequestFactory(enforce_csrf_checks=False)
+        view = ActivityViewSet.as_view({"post": "build"})
+        request = factory.post(
+            reverse("activities-list"),
+            activity_builder_data,
+            format="json",
+        )
+        force_authenticate(request, user=user)
+        return view(request)
+
+    def edit_activity(self, activity, user, data):
+        """
+        Edit an activity using the ActivityViewSet.
+
+        This method edits an activity by sending a PATCH request to the 'activity-detail' endpoint
+        with the provided activity data in JSON format. The request is authenticated with the provided user,
+        and the response is returned.
+        """
+        log.info("Editing activity")
+        factory = APIRequestFactory(enforce_csrf_checks=False)
+        view = ActivityViewSet.as_view({"patch": "partial_update"})
+
+        request = factory.patch(
+            reverse("activities-detail", args=[activity.id]),
+            data,
+            format="json",
+        )
+        force_authenticate(request, user=user)
+        return view(request, pk=activity.id)
+
+    def edit_module(self, module, user, data):
+        """
+        Edit a module using the ModuleViewSet.
+
+        This method edits a module by sending a PATCH request to the 'module-detail' endpoint
+        with the provided module data in JSON format. The request is authenticated with the provided user,
+        and the response is returned.
+        """
+        log.info("Editing module")
+        factory = APIRequestFactory(enforce_csrf_checks=False)
+        view = generic_module_viewset(module.__class__).as_view({"patch": "partial_update"})
+        # Get viewset url name
+        request = factory.patch(
+            reverse("perennial-croplands-detail", args=[module.id]),
+            data,
+            format="json",
+        )
+        force_authenticate(request, user=user)
+        return view(request, pk=module.id)
+
     def test_create_project(self):
         """
         Test the creation of a project using the ProjectViewSet.
@@ -63,16 +186,7 @@ class ProjectLockTestCase(APITestCase):
         checked to ensure that the project is created successfully with a
         status code of 201 (Created).
         """
-        factory = APIRequestFactory(enforce_csrf_checks=False)
-        view = ProjectViewSet.as_view({"post": "create"})
-
-        request = factory.post(
-            reverse("project-list"),
-            self.project_data,
-            format="json",
-        )
-        force_authenticate(request, user=self.user)
-        response = view(request)
+        response = self.create_project()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_modify_project_as_not_lock_holder(self):
@@ -90,26 +204,11 @@ class ProjectLockTestCase(APITestCase):
         - The project creation response status code is `HTTP_201_CREATED`.
         - The project modification response status code is `HTTP_400_BAD_REQUEST`.
         """
-        factory = APIRequestFactory(enforce_csrf_checks=False)
-        create_view = ProjectViewSet.as_view({"post": "create"})
 
-        create_request = factory.post(
-            reverse("project-list"),
-            self.project_data,
-            format="json",
-        )
-        force_authenticate(create_request, user=self.user)
-        create_response = create_view(create_request)
-        self.assertEqual(create_response.status_code, status.HTTP_201_CREATED)
-
+        create_response = self.create_project()
         project = models.Project.objects.get(id=create_response.data["id"])
-
-        modify_view = ProjectViewSet.as_view({"patch": "partial_update"})
-        models.ProjectMembership.objects.create(user=self.user2, project=project, group=self.group)
-
-        modify_request = factory.patch(reverse("project-detail", args=[project.id]), {"name": "New Name"}, format="json")
-        force_authenticate(modify_request, user=self.user2)
-        modify_response = modify_view(modify_request, pk=project.id)
+        self.create_project_membership(project, self.user2)
+        modify_response = self.edit_project(project, self.user2, {"name": "New Name"})
         self.assertEqual(modify_response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_modify_project_as_lock_holder(self):
@@ -126,24 +225,128 @@ class ProjectLockTestCase(APITestCase):
         7. Verifies that the project is modified successfully with a 200 status code.
         8. Confirms that the project's name has been updated to "New Name".
         """
-        factory = APIRequestFactory(enforce_csrf_checks=False)
-        create_view = ProjectViewSet.as_view({"post": "create"})
 
-        create_request = factory.post(
-            reverse("project-list"),
-            self.project_data,
-            format="json",
-        )
+        create_response = self.create_project()
 
-        force_authenticate(create_request, user=self.user)
-        create_response = create_view(create_request)
         self.assertEqual(create_response.status_code, status.HTTP_201_CREATED)
 
         project = models.Project.objects.get(id=create_response.data["id"])
+        modify_response = self.edit_project(project, self.user, {"name": "New Name"})
 
-        modify_view = ProjectViewSet.as_view({"patch": "partial_update"})
-        modify_request = factory.patch(reverse("project-detail", args=[project.id]), {"name": "New Name"}, format="json")
-        force_authenticate(modify_request, user=self.user)
-        modify_response = modify_view(modify_request, pk=project.id)
         self.assertEqual(modify_response.status_code, status.HTTP_200_OK)
         self.assertEqual(modify_response.data["name"], "New Name")
+
+    def test_modify_actvity_as_not_lock_holder(self):
+        """
+        Test that a user who is not the lock holder of an activity cannot modify it.
+
+        Steps:
+        1. Create a project.
+        2. Verify the project creation response status is HTTP 201 Created.
+        3. Retrieve the created project from the database.
+        4. Create a project membership for a second user.
+        5. Create an activity within the project by the first user.
+        6. Verify the activity creation response status is HTTP 200 OK.
+        7. Retrieve the created activity from the database.
+        8. Attempt to modify the activity by the second user.
+        9. Verify the modification response status is HTTP 400 Bad Request.
+        """
+
+        create_project_response = self.create_project()
+        self.assertEqual(create_project_response.status_code, status.HTTP_201_CREATED)
+        project = models.Project.objects.get(id=create_project_response.data["id"])
+        self.create_project_membership(project, self.user2)
+
+        create_activity_response = self.create_activity(project, self.user)
+        self.assertEqual(create_activity_response.status_code, status.HTTP_200_OK)
+        activity = models.Activity.objects.get(id=create_activity_response.data["id"])
+
+        modify_activity_response = self.edit_activity(activity, self.user2, {"name": "New Name"})
+        self.assertEqual(modify_activity_response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_modify_activity_as_lock_holder(self):
+        """
+        Test modifying an activity as the lock holder.
+
+        This test performs the following steps:
+        1. Creates a project and verifies the response status code is 201 (Created).
+        2. Retrieves the created project from the database.
+        3. Creates an activity within the project and verifies the response status code is 200 (OK).
+        4. Retrieves the created activity from the database.
+        5. Modifies the activity's name and verifies the response status code is 200 (OK).
+        6. Confirms that the activity's name has been updated to the new value.
+
+        Assertions:
+        - The project creation response status code is 201.
+        - The activity creation response status code is 200.
+        - The activity modification response status code is 200.
+        - The activity's name is updated to "New Name".
+        """
+
+        create_project_response = self.create_project()
+        self.assertEqual(create_project_response.status_code, status.HTTP_201_CREATED)
+        project = models.Project.objects.get(id=create_project_response.data["id"])
+
+        create_activity_response = self.create_activity(project, self.user)
+        self.assertEqual(create_activity_response.status_code, status.HTTP_200_OK)
+        activity = models.Activity.objects.get(id=create_activity_response.data["id"])
+
+        modify_activity_response = self.edit_activity(activity, self.user, {"name": "New Name"})
+        self.assertEqual(modify_activity_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(modify_activity_response.data["name"], "New Name")
+
+    def test_modify_module_as_not_lock_holder(self):
+        """
+        Test that a user who is not the lock holder of a module cannot modify it.
+
+        This test performs the following steps:
+        1. Creates a project and verifies the project creation.
+        2. Adds a second user as a member of the project.
+        3. Creates an activity within the project and verifies the activity creation.
+        4. Attempts to modify a module within the activity using the second user.
+        5. Verifies that the modification attempt fails with a 400 Bad Request status code.
+
+        The test ensures that only the lock holder of a module can modify it.
+        """
+
+        create_project_response = self.create_project()
+        self.assertEqual(create_project_response.status_code, status.HTTP_201_CREATED)
+        project = models.Project.objects.get(id=create_project_response.data["id"])
+        self.create_project_membership(project, self.user2)
+
+        create_activity_response = self.create_activity(project, self.user)
+        self.assertEqual(create_activity_response.status_code, status.HTTP_200_OK)
+        activity = models.Activity.objects.get(id=create_activity_response.data["id"])
+
+        module = activity.modules[0]
+
+        response = self.edit_module(module, self.user2, {"area": 50})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_modify_module_as_lock_holder(self):
+        """
+        Test modifying a module as the lock holder.
+        This test case verifies that a user who holds the lock on a project can successfully modify a module within that project.
+        Steps:
+        1. Create a project and verify the response status is HTTP 201 Created.
+        2. Retrieve the created project from the database.
+        3. Create an activity within the project and verify the response status is HTTP 200 OK.
+        4. Retrieve the created activity from the database.
+        5. Select the first module from the activity.
+        6. Attempt to edit the module's area attribute as the lock holder and verify the response status is HTTP 200 OK.
+        7. Verify that the module's area attribute has been updated to the new value.
+        """
+
+        create_project_response = self.create_project()
+        self.assertEqual(create_project_response.status_code, status.HTTP_201_CREATED)
+        project = models.Project.objects.get(id=create_project_response.data["id"])
+
+        create_activity_response = self.create_activity(project, self.user)
+        self.assertEqual(create_activity_response.status_code, status.HTTP_200_OK)
+        activity = models.Activity.objects.get(id=create_activity_response.data["id"])
+
+        module = activity.modules[0]
+
+        response = self.edit_module(module, self.user, {"area": 50})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["area"], 50)
