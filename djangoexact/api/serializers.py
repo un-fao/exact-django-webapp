cfa01c09f7bb09b772cd61ccba22acdb8de2d0f4
Claudio Lavacca <claudio.lavacca@fao.org>
2024-11-20 16:35:02 +0100

    Fix missing context in serializer

--- djangoexact/api/serializers.py
+++ djangoexact/api/serializers.py
@@ -2054,7 +2054,7 @@ class FuelSerializer(ScenarioSubmoduleSerializer):
         if parent.fuels.count() + 1 > max_elements:
             raise serializers.ValidationError(f"Only {max_elements} fuel modules are allowed")
 
-        parent_serializer = EnergySerializer(data={}, instance=parent, partial=True)
+        parent_serializer = EnergySerializer(data={}, instance=parent, partial=True, context=self.context)
         if parent_serializer.is_valid():
             parent_serializer.save()
 
@@ -2109,7 +2109,7 @@ class ElectricityWriteSerializer(NoScenarioSubmoduleSerializer):
         if not self.instance and parent.electricities.count() + 1 > max_elements:
             raise serializers.ValidationError(f"Only {max_elements} electricity modules are allowed")
 
-        parent_serializer = EnergySerializer(data={}, instance=parent, partial=True)
+        parent_serializer = EnergySerializer(data={}, instance=parent, partial=True, context=self.context)
         if parent_serializer.is_valid():
             parent_serializer.save()
 
