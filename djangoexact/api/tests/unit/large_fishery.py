7249681f21d5f59511d5de68a3b014f40ffb12ea
Claudio Lavacca <claudio.lavacca@fao.org>
2024-11-25 16:32:02 +0100

    Add fisheries unit tests

---
+++ djangoexact/api/tests/unit/large_fishery.py
@@ -0,0 +1,77 @@
+from rest_framework.test import APIRequestFactory, APITestCase
+from rest_framework import status
+from django.urls import reverse
+from api.views import generic_module_viewset
+import api.models as models
+import ipcc.models as ipcc_models
+import api.tests.factories as factories
+from rest_framework.test import force_authenticate
+from factory.fuzzy import FuzzyText, FuzzyInteger, FuzzyChoice, FuzzyFloat
+import logging as log
+from api.tests.unit.utils import APITestCaseMixin
+from api import serializers
+import copy
+from . import base_module
+
+
+class LargeFisheryTestCase(base_module.BaseModuleTestCase):
+    def setUp(self):
+        self.ModuleClass = models.LargeFishery
+        super().setUp()
+
+        self.land_use_types = self.land_use_types.filter(module_types__class_name=self.ModuleClass.__name__, climates=self.project.climate, moistures=self.project.moisture, is_active=True)
+
+        self.validated_data = {
+            "gear_type_start": models.LargeFisheryGearType.objects.order_by("?").first().id,
+            "gear_type_w": models.LargeFisheryGearType.objects.order_by("?").first().id,
+            "gear_type_wo": models.LargeFisheryGearType.objects.order_by("?").first().id,
+            "total_catch_yr_start": FuzzyInteger(1, 100).fuzz(),
+            "total_catch_yr_w": FuzzyInteger(1, 100).fuzz(),
+            "total_catch_yr_wo": FuzzyInteger(1, 100).fuzz(),
+            "fish_type": models.FishType.objects.order_by("?").first().id,
+        }
+
+        self.edit_module(self.module, self.user, self.validated_data)
+        self.module.refresh_from_db()
+
+    def test_modify_annual_cropland(self):
+
+        validated_data = copy.deepcopy(self.validated_data)
+        validated_data["fish_type"] = models.FishType.objects.order_by("?").first().id
+        response = self.edit_module(self.module, self.user, validated_data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["status"]["name"], "READY")
+
+    def test_patch_annual_cropland_to_not_ready(self):
+
+        validated_data = copy.deepcopy(self.validated_data)
+        validated_data["fish_type"] = None
+        response = self.edit_module(self.module, self.user, validated_data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["status"]["name"], "EMPTY")
+
+    def test_calculate_annual_cropland_results(self):
+
+        view = self.module_viewset.as_view({"get": "results"})
+        request = self.request_factory.get(reverse(f"{self.ModuleClass.__name__.lower()}-results", args=[self.module.pk]), format="json")
+
+        force_authenticate(request, user=self.user)
+        response = view(request, pk=self.module.pk)
+        print(response.data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue("balance" in response.data)
+
+    def test_get_annual_cropland_defaults(self):
+
+        view = self.module_viewset.as_view({"get": "defaults"})
+        request = self.request_factory.get(reverse(f"{self.ModuleClass.__name__.lower()}-defaults", args=[self.module.pk]), format="json")
+
+        force_authenticate(request, user=self.user)
+        response = view(request, pk=self.module.pk)
+        print(response.data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(type(response.data) == dict)
