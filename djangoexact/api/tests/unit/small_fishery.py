50341ef35f5422cbc1f4d68958042b0ad419a452
Claudio Lavacca <claudio.lavacca@fao.org>
2024-11-25 17:19:45 +0100

    Refactor unit tests

--- djangoexact/api/tests/unit/small_fishery.py
+++ djangoexact/api/tests/unit/small_fishery.py
@@ -34,7 +34,7 @@ class SmallFisheryTestCase(base_module.BaseModuleTestCase):
         self.edit_module(self.module, self.user, self.validated_data)
         self.module.refresh_from_db()
 
-    def test_modify_annual_cropland(self):
+    def test_modify(self):
 
         validated_data = copy.deepcopy(self.validated_data)
         validated_data["fishery_type"] = models.FisheryType.objects.order_by("?").first().id
@@ -43,7 +43,7 @@ class SmallFisheryTestCase(base_module.BaseModuleTestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["status"]["name"], "READY")
 
-    def test_patch_annual_cropland_to_not_ready(self):
+    def test_patch_to_not_ready(self):
 
         validated_data = copy.deepcopy(self.validated_data)
         validated_data["fishery_type"] = None
@@ -52,7 +52,7 @@ class SmallFisheryTestCase(base_module.BaseModuleTestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["status"]["name"], "EMPTY")
 
-    def test_calculate_annual_cropland_results(self):
+    def test_calculate_results(self):
 
         view = self.module_viewset.as_view({"get": "results"})
         request = self.request_factory.get(reverse(f"{self.ModuleClass.__name__.lower()}-results", args=[self.module.pk]), format="json")
@@ -64,7 +64,7 @@ class SmallFisheryTestCase(base_module.BaseModuleTestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue("balance" in response.data)
 
-    def test_get_annual_cropland_defaults(self):
+    def test_get_defaults(self):
 
         view = self.module_viewset.as_view({"get": "defaults"})
         request = self.request_factory.get(reverse(f"{self.ModuleClass.__name__.lower()}-defaults", args=[self.module.pk]), format="json")
