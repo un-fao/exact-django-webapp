9940cf0379a1d7e80bf7954c774ee739ae175532
Claudio Lavacca <claudio.lavacca@fao.org>
2024-11-26 12:34:36 +0100

    Refactor unit tests

--- djangoexact/api/tests/unit/coastal_wetland.py
+++ djangoexact/api/tests/unit/coastal_wetland.py
@@ -19,11 +19,18 @@ class CoastalWetlandTestCase(base_module.BaseModuleTestCase):
         self.ModuleClass = models.CoastalWetland
         super().setUp()
 
-        self.land_use_types = self.land_use_types.filter(module_types__class_name=self.ModuleClass.__name__, climates=self.project.climate, moistures=self.project.moisture, is_active=True)
+        print(f"Land use types: {self.land_use_types}")
 
         self.validated_data = {
             "land_use_type": self.land_use_types.order_by("?").first().id,
             "area": FuzzyInteger(1, 100).fuzz(),
+            "area_under_drainage_start": FuzzyInteger(1, 100).fuzz(),
+            "area_under_drainage_w": FuzzyInteger(1, 100).fuzz(),
+            "area_under_drainage_wo": FuzzyInteger(1, 100).fuzz(),
+            # NOTE: Added to avoid issues with IPCC missing data
+            "co2_rewetting_t2_start": FuzzyFloat(0, 100).fuzz(),
+            "co2_rewetting_t2_w": FuzzyFloat(0, 100).fuzz(),
+            "co2_rewetting_t2_wo": FuzzyFloat(0, 100).fuzz(),
         }
 
         self.edit_module(self.module, self.user, self.validated_data)
@@ -37,6 +44,43 @@ class CoastalWetlandTestCase(base_module.BaseModuleTestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["status"]["name"], "READY")
+
+    def test_modify_and_check_cache_invalidation(self):
+        self.test_modify()
+
+        # Check that the cache is invalidated
+        view = self.module_viewset.as_view({"get": "results"})
+        request = self.request_factory.get(reverse(f"{self.ModuleClass.__name__.lower()}-results", args=[self.module.pk]), format="json")
+
+        force_authenticate(request, user=self.user)
+        response = view(request, pk=self.module.pk)
+        print(response.data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue("balance" in response.data)
+
+        old_balance = response.data["balance"]
+
+        validated_data = copy.deepcopy(self.validated_data)
+        validated_data["area_under_drainage_w"] = 10000
+        response = self.edit_module(self.module, self.user, validated_data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["status"]["name"], "READY")
+
+        # Check that the cache is invalidated
+        view = self.module_viewset.as_view({"get": "results"})
+        request = self.request_factory.get(reverse(f"{self.ModuleClass.__name__.lower()}-results", args=[self.module.pk]), format="json")
+
+        force_authenticate(request, user=self.user)
+        response = view(request, pk=self.module.pk)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue("balance" in response.data)
+
+        new_balance = response.data["balance"]
+
+        self.assertNotEqual(old_balance, new_balance)
 
     def test_patch_to_not_ready(self):
 
--- djangoexact/api/tests/unit/perennial_cropland.py
+++ djangoexact/api/tests/unit/perennial_cropland.py
@@ -6,7 +6,7 @@ import api.models as models
 import ipcc.models as ipcc_models
 import api.tests.factories as factories
 from rest_framework.test import force_authenticate
-from factory.fuzzy import FuzzyText, FuzzyInteger, FuzzyChoice
+from factory.fuzzy import FuzzyText, FuzzyInteger, FuzzyChoice, FuzzyFloat
 import logging as log
 from api.tests.unit.utils import APITestCaseMixin
 from api import serializers
@@ -33,6 +33,18 @@ class PerennialCroplandTestCase(base_module.BaseModuleTestCase):
             "is_biomass_burned_start": FuzzyChoice([True, False]).fuzz(),
             "is_biomass_burned_w": FuzzyChoice([True, False]).fuzz(),
             "is_biomass_burned_wo": FuzzyChoice([True, False]).fuzz(),
+            "biomass_t2_start": FuzzyFloat(0, 100).fuzz(),
+            "biomass_t2_w": FuzzyFloat(0, 100).fuzz(),
+            "biomass_t2_wo": FuzzyFloat(0, 100).fuzz(),
+            "agb_t2_start": FuzzyFloat(0, 100).fuzz(),
+            "agb_t2_w": FuzzyFloat(0, 100).fuzz(),
+            "agb_t2_wo": FuzzyFloat(0, 100).fuzz(),
+            "bgb_t2_start": FuzzyFloat(0, 100).fuzz(),
+            "bgb_t2_w": FuzzyFloat(0, 100).fuzz(),
+            "bgb_t2_wo": FuzzyFloat(0, 100).fuzz(),
+            "agb_max_t2_start": FuzzyFloat(0, 100).fuzz(),
+            "agb_max_t2_w": FuzzyFloat(0, 100).fuzz(),
+            "agb_max_t2_wo": FuzzyFloat(0, 100).fuzz(),
         }
 
         self.edit_module(self.module, self.user, self.validated_data)
@@ -46,6 +58,43 @@ class PerennialCroplandTestCase(base_module.BaseModuleTestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["status"]["name"], "READY")
+
+    def test_modify_and_check_cache_invalidation(self):
+        self.test_modify()
+
+        # Check that the cache is invalidated
+        view = self.module_viewset.as_view({"get": "results"})
+        request = self.request_factory.get(reverse(f"{self.ModuleClass.__name__.lower()}-results", args=[self.module.pk]), format="json")
+
+        force_authenticate(request, user=self.user)
+        response = view(request, pk=self.module.pk)
+        print(response.data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue("balance" in response.data)
+
+        old_balance = response.data["balance"]
+
+        validated_data = copy.deepcopy(self.validated_data)
+        validated_data["agb_t2_w"] = 100000
+        response = self.edit_module(self.module, self.user, validated_data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["status"]["name"], "READY")
+
+        # Check that the cache is invalidated
+        view = self.module_viewset.as_view({"get": "results"})
+        request = self.request_factory.get(reverse(f"{self.ModuleClass.__name__.lower()}-results", args=[self.module.pk]), format="json")
+
+        force_authenticate(request, user=self.user)
+        response = view(request, pk=self.module.pk)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue("balance" in response.data)
+
+        new_balance = response.data["balance"]
+
+        self.assertNotEqual(old_balance, new_balance)
 
     def test_patch_to_not_ready(self):
 
