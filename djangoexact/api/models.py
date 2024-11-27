d037c7489f98bfa41e2e0e92b7811ee89fbe9f52
Claudio Lavacca <claudio.lavacca@fao.org>
2024-11-27 13:21:17 +0100

    Refactor testing to filter correct fuel types in irrigation factories

--- djangoexact/api/models.py
+++ djangoexact/api/models.py
@@ -496,7 +496,7 @@ class FuelUseType(models.Model):
 
 class FuelType(models.Model):
     name = models.CharField(max_length=100)
-    fuel_use_type = models.ForeignKey(FuelUseType, on_delete=models.CASCADE, null=True, blank=True)
+    fuel_use_type = models.ForeignKey(FuelUseType, on_delete=models.CASCADE, related_name="fuel_types")
     macro_fuel_type = models.ForeignKey(MacroFuelType, on_delete=models.CASCADE, null=True, blank=True)
     module_types = models.ManyToManyField(ModuleType, related_name="fuel_types")
 
@@ -504,8 +504,6 @@ class FuelType(models.Model):
         unique_together = ("name", "fuel_use_type", "macro_fuel_type")
 
     def __str__(self):
-        macro = getattr(self.macro_fuel_type, "name", None)
-        use = getattr(self.fuel_use_type, "name", None)
         return self.name
 
 
