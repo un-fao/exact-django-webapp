2890ee51297985dd79c9d0f57fa86e349ab653a4
peterwright95 <peter.wright95@gmail.com>
2024-12-23 14:07:47 +0100

    Remove plots and fix logic

--- djangoexact/math_model/no_time_dependency_final/testing.py
+++ djangoexact/math_model/no_time_dependency_final/testing.py
@@ -1,12 +1,4 @@
-from general_functions import yearly_time_dependent_full_year
+from general_functions import compute_yearly_cumulative_matrix, plot_matrix_with_values
 
-
-
-start_value = 0 
-end_value = 100
-years_implementation = 5
-years_capitalization = 10
-function = 'linear'
-
-print(yearly_time_dependent_full_year(start_value, end_value, years_implementation, years_capitalization, function))
-
+ao = compute_yearly_cumulative_matrix(10000, 0, 10, 5, 'exponential')
+plot_matrix_with_values(ao,)
