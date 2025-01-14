a0b78684a510605a524b44851fb9cd355df60f95
Claudio Lavacca (CSI) <claudio.lavacca@fao.org>
2025-01-14 12:19:19 +0100

    Cut logic forcing agroforestry default values for default land use type biomass

--- djangoexact/api/models.py
+++ djangoexact/api/models.py
@@ -1297,10 +1297,6 @@ class SingleBiomassModule(BiomassModule):
         if land_use_type is None:
             raise ValueError(f"Missing land use type for {scenario.value} scenario")
 
-        # BUG: This is a temporary fix because the land use type "Default" is not being used in the database for biomass data. Unify this.
-        if land_use_type.name_en == "Default":
-            land_use_type = LandUseType.objects.get(name_en="Agroforestry - Default")
-
         try:
             return BiomassModel.objects.get(climate=climate, moisture=moisture, continent=continent, land_use_type=land_use_type)
         except BiomassModel.DoesNotExist:
