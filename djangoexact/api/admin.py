3ee1c3d1e201a9030b709f60f1f148e3369e9cff
Claudio Lavacca (CSI) <claudio.lavacca@fao.org>
2025-01-15 18:16:06 +0100

    Refactor status check endpoint name to health

--- djangoexact/api/admin.py
+++ djangoexact/api/admin.py
@@ -4,7 +4,7 @@ from .models import *
 from unfold.contrib.filters.admin import RangeDateTimeFilter
 
 
-for model in [model for model in dir() if not model.startswith("_") and model != "FieldDefinition" and model != "APIStatus"]:
+for model in [model for model in dir() if not model.startswith("_") and model != "FieldDefinition" and model != "APIHealth"]:
     try:
         admin.site.register(eval(model), ModelAdmin)
     except:
@@ -16,7 +16,7 @@ class FieldDefinitionAdmin(ModelAdmin):
     list_display = ("module_type", "field_name", "description")
     search_fields = ("module_type", "field_name")
 
-@admin.register(APIStatus)
+@admin.register(APIHealth)
 class APIStatusAdmin(ModelAdmin):
     list_display = ('is_under_maintenance', 'maintenance_end_time')
     list_filter_submit = True  # Submit button at the bottom of the filter
@@ -26,7 +26,7 @@ class APIStatusAdmin(ModelAdmin):
 
     # Ensure only one APIStatus instance exists
     def has_add_permission(self, request):
-        return not APIStatus.objects.exists()
+        return not APIHealth.objects.exists()
     
     def has_delete_permission(self, request, obj=None):
         return False
