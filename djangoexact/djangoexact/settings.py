eaffd04761f1502723c305644f510daa5c7f7c43
Claudio Lavacca (CSI) <claudio.lavacca@fao.org>
2025-01-20 11:50:02 +0100

    Add env config switch

--- djangoexact/djangoexact/settings.py
+++ djangoexact/djangoexact/settings.py
@@ -23,6 +23,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+app_mode = os.getenv("APP_MODE", "development")
+print(f"Running in {app_mode} mode")
+
+dotenv_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), "djangoexact", f".env.{app_mode}")
+
+load_dotenv(dotenv_file)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
