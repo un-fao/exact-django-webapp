867583c8daf6909b8cfd167cef5d33d4c2f27a26
Claudio Lavacca (CSI) <claudio.lavacca@fao.org>
2025-02-05 15:14:55 +0100

    Add perennial cropland comment to report

--- djangoexact/api/reports.py
+++ djangoexact/api/reports.py
@@ -979,6 +979,11 @@ class PerennialCroplandReport(LandModuleReport):
             self.metadata_worksheet.cell(row=last_metadata_row + 4, column=4, value=self.module.organic_input_type_wo.name)
             self.metadata_worksheet.cell(row=last_metadata_row + 5, column=4, value=self.module.crop_yield_t2_wo if self.module.crop_yield_t2_wo is not None else "Default")
 
+        self.metadata_worksheet.cell(row=last_metadata_row + 2, column=6, value=self.module.land_use_type_thread.format_comments())
+        self.metadata_worksheet.cell(row=last_metadata_row + 3, column=6, value=self.module.tillage_management_type_thread.format_comments())
+        self.metadata_worksheet.cell(row=last_metadata_row + 4, column=6, value=self.module.organic_input_type_thread.format_comments())
+        self.metadata_worksheet.cell(row=last_metadata_row + 5, column=6, value=self.module.crop_yield_t2_thread.format_comments())
+
         self.activity_report.project_report.excel_manager.save_workbook(self.workbook)
 
     def populate_additional_indicators(self):
