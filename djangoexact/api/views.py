9e8f098b701045b394201c2be32b59b444f39420
Claudio Lavacca (CSI) <claudio.lavacca@fao.org>
2025-03-12 14:13:42 +0100

    Fix project membership double delete

--- djangoexact/api/views.py
+++ djangoexact/api/views.py
@@ -1251,8 +1251,8 @@ class ProjectMembershipViewSet(viewsets.ModelViewSet, AuthenticatedViewSet):
     def destroy(self, request, *args, **kwargs):
         membership = self.get_object()
 
-        if membership.user == self.request.user and membership.project.owner != self.request.user:
-            membership.delete()
+        if membership.user == self.request.user and membership.project.owner == self.request.user:
+            return utils.ErrorResponse("Project owner cannot delete their own membership. Delete the project instead.", status=http_status.HTTP_400_BAD_REQUEST)
 
         elif not utils.has_project_permission("delete_projectmembership", self.request.user, membership.project):
             logging.error("Selected user does not have permission to delete project memberships")
