8fed52efd70917a34b4019c2ac6f9c2574450a03
Claudio Lavacca (CSI) <claudio.lavacca@fao.org>
2025-04-09 13:38:07 +0200

    fix: check that new last year of accounting for the project is not lower than the duretion of its activities

--- djangoexact/api/tests/unit/project.py
+++ djangoexact/api/tests/unit/project.py
@@ -177,6 +177,8 @@ class ProjectTestCase(APITestCaseMixin):
         7. Verify that the module's area attribute has been updated to the new value.
         """
 
+        log.info("START - test_modify_module_as_lock_holder")
+
         create_project_response = self.create_project()
         self.assertEqual(create_project_response.status_code, status.HTTP_201_CREATED)
         project = models.Project.objects.get(id=create_project_response.data["id"])
@@ -190,6 +192,8 @@ class ProjectTestCase(APITestCaseMixin):
         response = self.edit_module(module, self.user, {"area": 50})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["area"], 50)
+
+        log.info("END - test_modify_module_as_lock_holder")
 
     def test_archive_project_and_modify(self):
         """
@@ -311,3 +315,38 @@ class ProjectTestCase(APITestCaseMixin):
 
         modify_response = self.edit_project(project, self.user2, {"is_archived": True})
         self.assertEqual(modify_response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_changing_last_year_of_accounting_for_project_with_activity_having_duration_t2_exceeding_last_year_of_accounting(self):
+        """
+        Test that changing the last year of accounting for a project with an activity having a duration exceeding the last year of accounting is not allowed.
+
+        This test performs the following steps:
+        1. Creates a project and verifies the project creation.
+        2. Creates an activity within the project and verifies the activity creation.
+        3. Attempts to change the last year of accounting for the project and verifies that the modification attempt fails with a 400 Bad Request status code.
+
+        The test ensures that changing the last year of accounting for a project with an activity having a duration exceeding the last year of accounting is not allowed.
+        """
+
+        log.info("START - test_changing_last_year_of_accounting_for_project_with_activity_having_duration_t2_exceeding_last_year_of_accounting")
+
+        self.project_data["start_year_of_activities"] = 2000
+        self.project_data["implementation_years"] = 10
+        self.project_data["last_year_of_accounting"] = 2020
+
+        create_project_response = self.create_project()
+        self.assertEqual(create_project_response.status_code, status.HTTP_201_CREATED)
+        project = models.Project.objects.get(id=create_project_response.data["id"])
+
+        create_activity_response = self.create_activity(project, self.user)
+        self.assertEqual(create_activity_response.status_code, status.HTTP_200_OK)
+        activity = models.Activity.objects.get(id=create_activity_response.data["id"])
+
+        edit_activity_response = self.edit_activity(activity, self.user, {"duration_t2": 10})
+        self.assertEqual(edit_activity_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(edit_activity_response.data["duration_t2"], 10)
+
+        edit_project_response = self.edit_project(project, self.user, {"last_year_of_accounting": 2009})
+        self.assertEqual(edit_project_response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        log.info("END - test_changing_last_year_of_accounting_for_project_with_activity_having_duration_t2_exceeding_last_year_of_accounting")
