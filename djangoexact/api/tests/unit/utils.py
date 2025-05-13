ee7e35b1a0b9ddb40465fee2053cd32ee6f32a7a
Claudio Lavacca (CSI) <claudio.lavacca@fao.org>
2025-05-13 15:40:40 +0100

    feat: add excel and templated reports to public project viewset

--- djangoexact/api/tests/unit/utils.py
+++ djangoexact/api/tests/unit/utils.py
@@ -364,3 +364,21 @@ class APITestCaseMixin(APITestCase):
             format="json",
         )
         return view(request, pk=activity.id)
+
+    def get_report_anonimously(self, project, templated=False):
+        """
+        Get project report data without authentication.
+        This method retrieves project report data by sending a GET request to the 'project-report' endpoint
+        with the provided project ID. The request is not authenticated, and the response is returned.
+        """
+        log.info("Getting project report data without authentication")
+        view = public_views.PublicProjectViewSet.as_view({"get": "report"})
+        queryparams = None
+        if templated:
+            queryparams = {"template": "fao"}
+        request = self.request_factory.get(
+            reverse("project-detail", args=[project.id]),
+            queryparams,
+            format="json",
+        )
+        return view(request, pk=project.id)
