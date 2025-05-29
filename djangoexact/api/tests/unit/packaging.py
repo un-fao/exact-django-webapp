812c7c845929705454a9e7fdc9675f653cdfc0e4
Claudio Lavacca (CSI) <claudio.lavacca@fao.org>
2025-05-29 14:18:51 +0100

    feat: add value chain modules reports

--- djangoexact/api/tests/unit/packaging.py
+++ djangoexact/api/tests/unit/packaging.py
@@ -120,3 +120,10 @@ class PackagingTestCase(base_module.BaseModuleWithSubmoduleTestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue("balance" in response.data)
         self.assertNotEqual(response.data["balance"], first_balance)
+
+    def test_excel_report(self):
+        self.test_calculate_results()
+        self.module.refresh_from_db()
+        response = self.get_report(self.project, self.user)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", response["Content-Type"])
