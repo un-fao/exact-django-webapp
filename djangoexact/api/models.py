a2a5eaa3e414f66d5a2c78dd1ae353b07c8fd1bc
Claudio Lavacca (CSI) <claudio.lavacca@fao.org>
2025-07-16 15:18:53 +0200

    feat: add fishery type m2m link in the small fishery gear type model

--- djangoexact/api/models.py
+++ djangoexact/api/models.py
@@ -509,6 +509,7 @@ class LargeFisheryGearType(models.Model):
 
 class SmallFisheryGearType(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    fishery_type = models.ManyToManyField(FisheryType, related_name="gear_types", null=True, blank=True)
 
     def __str__(self):
         return self.name
