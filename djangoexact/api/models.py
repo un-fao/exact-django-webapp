08aada64688356e3e9903e25e456b837d3b678bd
Claudio Lavacca (CSI) <claudio.lavacca@fao.org>
2025-09-19 15:07:26 +0200

    ref(SingleBiomassModule): use get_or_default method to fetch biomass data

--- djangoexact/api/models.py
+++ djangoexact/api/models.py
@@ -1472,14 +1472,11 @@ class SingleBiomassModule(BiomassModule):
             raise ValueError(f"Missing land use type for {scenario.value} scenario")
 
         try:
-            return BiomassModel.objects.get(climate=climate, moisture=moisture, continent=continent, land_use_type=land_use_type)
+            return BiomassModel.objects.get_or_default(climate=climate, moisture=moisture, continent=continent, land_use_type=land_use_type)
         except BiomassModel.DoesNotExist:
-            try:
-                return BiomassModel.objects.get(climate=climate, moisture=moisture, continent=continent, land_use_type=LandUseType.objects.get(class_name=self.__class__.__name__))
-            except BiomassModel.DoesNotExist:
-                if getattr(self, f"biomass_t2_{scenario.value}", None) is None:
-                    raise ValueError(f"Missing biomass data for {land_use_type}, {climate}, {moisture}, {continent}, for {scenario.verbose_name} scenario. Please provide tier2 value.")
-                return BiomassModel()
+            if getattr(self, f"biomass_t2_{scenario.value}", None) is None:
+                raise ValueError(f"Missing biomass data for {land_use_type}, {climate}, {moisture}, {continent}, for {scenario.verbose_name} scenario. Please provide tier2 value.")
+            return BiomassModel()
 
 
 class ResidueAvailability(models.Model):
