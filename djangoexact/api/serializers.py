de9d8f51cc00c8da42a4a68a2619c911326f07b0
Claudio Lavacca (CSI) <claudio.lavacca@fao.org>
2025-11-07 20:06:11 +0100

    fix(activity_builder): prevent luc module type from always being added

--- djangoexact/api/serializers.py
+++ djangoexact/api/serializers.py
@@ -972,7 +972,7 @@ class ActivityBuilderSerializer(serializers.Serializer):
             self.sanitize_input_entries()
 
             self.instance.module_types.add(*module_types_to_append)
-            if not was_luc_removed:
+            if (luc or was_luc_added) and not was_luc_removed:
                 self.instance.module_types.add(ModuleType.objects.get(class_name="LandUseChange").id)
 
             self.instance.save()
