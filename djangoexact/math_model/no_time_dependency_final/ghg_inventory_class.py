9da3b79dbe31c2db34505854859393c9f3fb8ea0
Claudio Lavacca (CSI) <claudio.lavacca@fao.org>
2025-11-10 12:05:52 +0100

    ref(math_model): refine inventory dictionary casts and class namespace definitions

--- djangoexact/math_model/no_time_dependency_final/ghg_inventory_class.py
+++ djangoexact/math_model/no_time_dependency_final/ghg_inventory_class.py
@@ -1,12 +1,19 @@
-
 from .ghg_emissions_classes import Emission, ActivityTypes
 from typing import List
 
+
 class InventoryPerGasperActivity(Emission):
     def __init__(self, gas_type, emission, activity):
-        super.init(gas_type,emission)
-        self.activity: ActivityTypes = activity  
+        super.init(emission, gas_type)
+        self.activity: ActivityTypes = activity
+
+    def to_dict(self):
+        return {"activity": self.activity.value, **super().to_dict()}
+
 
 class Inventory:
-    def __init__(self,):
+    def __init__(self):
         self.emission_by_sector_by_gas: List[InventoryPerGasperActivity] = []
+
+    def to_dict(self):
+        return [inventory.to_dict() for inventory in self.emission_by_sector_by_gas]
