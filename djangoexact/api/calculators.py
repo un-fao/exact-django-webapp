77ee7e254a86c83b5f13af4c56ebc6f4fe3291c6
Claudio Lavacca <claudio.lavacca@fao.org>
2026-05-18 10:24:17 +0200

    fix(calculators): extract salinity_type and soil_type_name into init of CoastalWetlandCalculator
    Fixes AttributeError thrown when downloading Excel report: the report never calls .get_defaults(), so the two attributes above were never set.

--- djangoexact/api/calculators.py
+++ djangoexact/api/calculators.py
@@ -6098,6 +6098,14 @@ class CoastalWetlandCalculator(BaseCalculator):
         self.rewetting_c = ipcc.RewettingCarbonFactor()
         self.rewetting_ch4 = ipcc.RewettingMethaneFactor()
 
+        self.salinity_type = SalinityType.objects.get(value="<18")
+        if self.module.avg_salinity_t2:
+            self.salinity_type = self.module.avg_salinity_t2
+
+        self.soil_type_name = "Mineral"
+        if self.module.soil_type_t2:
+            self.soil_type_name = self.module.soil_type_t2.name
+
         self.agb_default = ipcc.CoastalAGB()
         self.bgb_default = ipcc.CoastalBGB()
 
@@ -6115,9 +6123,6 @@ class CoastalWetlandCalculator(BaseCalculator):
             self.calculate()
             self.agb_default.value = getattr_or_default(self.math_w, "agb_tier_2_default") or getattr_or_default(self.math_wo, "agb_tier_2_default")
             self.bgb_default.value = getattr_or_default(self.math_w, "bgb_tier_2_default") or getattr_or_default(self.math_wo, "bgb_tier_2_default")
-
-        self.soil_type_name = self.module.soil_type_t2.name if self.module.soil_type_t2 else "Mineral"
-        self.salinity_type: SalinityType = self.module.avg_salinity_t2 if self.module.avg_salinity_t2 else SalinityType.objects.get(value="<18")
 
         try:
             self.agb = ipcc.CoastalAGB.objects.get(**cm, land_use_type=self.module.land_use_type)
